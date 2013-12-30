bbea2ca171efd954fcf92450337db0564c120169
bastimeyer <mail@bastimeyer.de>
2013-12-30 21:15:33 +0100

    Fixed content scrollborders... also changed styles

--- src/app/gui/scrollborders.js
+++ src/app/gui/scrollborders.js
@@ -2,26 +2,32 @@ define(function() {
 
 	return function() {
 		var	content		= document.body.querySelector( ".content" ),
+			elem		= null,
 			observer	= new MutationObserver(function( mutations ) {
 				content.classList.remove( "scrollborder-top" );
 				content.classList.remove( "scrollborder-bottom" );
 				mutations.forEach(function( mutation ) {
-					var elem = mutation.addedNodes[ 0 ];
-					if ( elem && elem.tagName === "SECTION" ) {
-						addScrollEvent( elem );
+					var el = mutation.addedNodes[ 0 ];
+					if ( el && el.tagName === "SECTION" ) {
+						addScrollEvent( el );
 					}
 				})
 			});
+
 		observer.observe( content, { childList: true } );
+
+		window.addEventListener( "resize", applyClasses, false );
 
 		addScrollEvent( content.querySelector( "section" ) );
 
-		function addScrollEvent( elem ) {
-			if ( !elem ) return;
-			elem.addEventListener( "scroll", onscroll.bind( null, elem ), false );
+		function addScrollEvent( el ) {
+			if ( !el ) return;
+			elem = el;
+			el.addEventListener( "scroll", applyClasses, false );
 		}
 
-		function onscroll( elem ) {
+		function applyClasses() {
+			if ( !elem ) return;
 			content.classList[ elem.scrollTop === 0
 				? "remove"
 				: "add" ]( "scrollborder-top" );
