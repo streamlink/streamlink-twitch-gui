633f566bf7d3ec084c0fe4c66fd4e591f2e994a5
bastimeyer <mail@bastimeyer.de>
2014-02-07 11:42:17 +0100

    Added a title to all channel list components containing the channel status

--- src/app/components/StreamItemComponent.js
+++ src/app/components/StreamItemComponent.js
@@ -7,6 +7,10 @@ define([
 		layout: Ember.Handlebars.compile( Template ),
 		tagName: "li",
 		classNames: [ "list-item list-item-stream" ],
+		attributeBindings: [ "title" ],
+		title: function() {
+			return this.get( "stream.channel.status" );
+		}.property( "stream.channel.status" ),
 
 		click: function() {
 			this.sendAction( "action", this.get( "stream" ) );
