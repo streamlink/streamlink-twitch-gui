5a43b8fab5afe4e8513c979a428d166f9c05c111
bastimeyer <mail@bastimeyer.de>
2014-05-06 12:57:19 +0200

    Updated build-dependencies and changed their order

--- Gruntfile.js
+++ Gruntfile.js
@@ -237,13 +237,13 @@ module.exports = function( grunt ) {
 	});
 
 
-	grunt.loadNpmTasks( "grunt-contrib-less" );
+	grunt.loadNpmTasks( "grunt-contrib-clean" );
+	grunt.loadNpmTasks( "grunt-contrib-compress" );
+	grunt.loadNpmTasks( "grunt-contrib-copy" );
 	grunt.loadNpmTasks( "grunt-contrib-jshint" );
+	grunt.loadNpmTasks( "grunt-contrib-less" );
 	grunt.loadNpmTasks( "grunt-contrib-requirejs" );
 	grunt.loadNpmTasks( "grunt-contrib-uglify" );
-	grunt.loadNpmTasks( "grunt-contrib-clean" );
-	grunt.loadNpmTasks( "grunt-contrib-copy" );
-	grunt.loadNpmTasks( "grunt-contrib-compress" );
 	grunt.loadNpmTasks( "grunt-node-webkit-builder" );
 	grunt.loadNpmTasks( "grunt-string-replace" );
 
