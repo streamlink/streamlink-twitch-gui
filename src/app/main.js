ed3f55bd2f068bb86fef7e4466fdc3711a638de5
bastimeyer <mail@bastimeyer.de>
2014-05-14 14:39:24 +0200

    Don't show the node-webkit exception page during debug mode

--- src/app/main.js
+++ src/app/main.js
@@ -2,6 +2,11 @@ if ( typeof DEBUG === "undefined" ) {
 	DEBUG = true;
 }
 
+if ( DEBUG ) {
+	// don't show the node-webkit exception page during debug mode
+	process.on( "uncaughtException", function() {} );
+}
+
 define(function( require ) {
 
 	require( [ "config" ], function() {
