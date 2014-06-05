20094de25de8f7ec868b8b7768b1ab9316669bb1
bastimeyer <mail@bastimeyer.de>
2014-06-05 11:55:32 +0200

    Simplified settings model

--- src/app/models/Settings.js
+++ src/app/models/Settings.js
@@ -1,17 +1,6 @@
-define( [ "ember", "ember-data" ], function( Ember, DS ) {
+define( [ "ember-data" ], function( DS ) {
 
-	var Settings = DS.Model.extend();
-	Settings.toString = function() { return "Settings"; };
-
-	var	qualities = [
-			{ id: 0, label: "best",   quality: "best" },
-			{ id: 1, label: "high",   quality: "high,mobile_high" },
-			{ id: 2, label: "medium", quality: "medium,mobile_medium" },
-			{ id: 3, label: "low",    quality: "low,mobile_mobile" },
-			{ id: 4, label: "worst",  quality: "worst" }
-		];
-
-	Settings.reopen({
+	return DS.Model.extend({
 		livestreamer	: DS.attr( "string", { defaultValue: "" } ),
 		quality			: DS.attr( "number", { defaultValue: 0 } ),
 		player			: DS.attr( "string", { defaultValue: "" } ),
@@ -19,9 +8,15 @@ define( [ "ember", "ember-data" ], function( Ember, DS ) {
 		player_reconnect: DS.attr( "boolean", { defaultValue: true } ),
 		player_no_close	: DS.attr( "boolean", { defaultValue: true } ),
 
-		qualities		: qualities
+		qualities		: [
+			{ id: 0, label: "best",   quality: "best" },
+			{ id: 1, label: "high",   quality: "high,mobile_high" },
+			{ id: 2, label: "medium", quality: "medium,mobile_medium" },
+			{ id: 3, label: "low",    quality: "low,mobile_mobile" },
+			{ id: 4, label: "worst",  quality: "worst" }
+		]
+	}).reopenClass({
+		toString: function() { return "Settings"; }
 	});
-
-	return Settings;
 
 });
