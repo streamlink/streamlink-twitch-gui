d97f658620a78071855f07363e71abea12d4720a
bastimeyer <mail@bastimeyer.de>
2014-06-18 10:04:24 +0200

    Added more lib paths to linux startscript. resolves #12

--- build/script/start.sh
+++ build/script/start.sh
@@ -20,12 +20,16 @@ if [[ ! -z `ldd $HERE/$EXEC | grep "libudev.so.0 => not found"` ]]; then
 	# check for an existing softlink
 	if [[ ! -e "$HERE/libudev.so.0" ]]; then
 		PATHS=(
-			"/lib/x86_64-linux-gnu/libudev.so.1" # Ubuntu, Xubuntu, Mint
-			"/usr/lib64/libudev.so.1" # SUSE, Fedora
-			"/usr/lib/libudev.so.1" # Arch, Fedora 32bit
-			"/lib/i386-linux-gnu/libudev.so.1" # Ubuntu 32bit
+			"/lib/x86_64-linux-gnu" # Ubuntu, Xubuntu, Mint
+			"/usr/lib64" # SUSE, Fedora
+			"/lib64" # Gentoo
+			"/lib/i386-linux-gnu" # Ubuntu 32bit
+			"/usr/lib" # Arch, Fedora 32bit
+			"/lib32" # Gentoo 32bit
+			"/usr/lib32" # Gentoo 32bit alternative
 		)
 		for i in "${PATHS[@]}"; do
+			i="$i/libudev.so.1"
 			if [[ -f $i ]]; then
 				# create the softlink to the needed library
 				ln -sf "$i" $HERE/libudev.so.0
