338ba7de89a82dccd75994b8c5fdfdc3cee0eccb
bastimeyer <mail@bastimeyer.de>
2014-08-05 19:08:22 +0200

    Use production builds of ember and ember-data for releases

--- build/tasks/configs/requirejs.js
+++ build/tasks/configs/requirejs.js
@@ -34,5 +34,19 @@ module.exports = {
 			generateSourceMaps	: true
 		}
 	},
-	release			: {}
+	release			: {
+		options			: {
+			/*
+			 * Do not use dev versions for release builds
+			 *
+			 * Defining two versions in the config file and mapping the names for a release build
+			 * doesn't seem to work here. So we need to overwrite the paths in the grunt config :(
+			 * https://gist.github.com/askesian/6e05daa443ca1955ea32
+			 */
+			paths			: {
+				"ember"			: "../vendor/ember/ember.prod",
+				"ember-data"	: "../vendor/ember-data/ember-data.prod"
+			}
+		}
+	}
 };
