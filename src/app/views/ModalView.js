e053104662b7b0e1d5930f7c351e9443504e1da4
bastimeyer <mail@bastimeyer.de>
2014-08-14 03:44:03 +0200

    Added dialog closing animation

--- src/app/views/ModalView.js
+++ src/app/views/ModalView.js
@@ -10,7 +10,26 @@ define([
 
 		SelectView: Ember.Select.extend({
 			classNameBindings: [ "myclass" ]
-		})
+		}),
+
+		didInsertElement: function() {
+			var $this = this.$();
+			this._super.apply( this, arguments );
+			Ember.run.next( $this, $this.addClass, "shown" );
+		},
+
+		/*
+		 * This will be called synchronous.
+		 * Ember doesn't support animations right now.
+		 * So we need to use an ugly hack :(
+		 */
+		willDestroyElement: function() {
+			var	$this	= this.$(),
+				$clone	= $this.clone();
+			$this.parent().append( $clone );
+			$clone.one( "webkitTransitionEnd", function() { $clone.remove(); });
+			Ember.run.next( $clone, $clone.removeClass, "shown" );
+		}
 	});
 
 });
