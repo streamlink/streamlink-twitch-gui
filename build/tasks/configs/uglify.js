47f4becb53245131ed1f63f9e6d2cdd82a1c49d9
bastimeyer <mail@bastimeyer.de>
2014-08-23 10:18:40 +0200

    Also uglify requirejs besides the main app code

--- build/tasks/configs/uglify.js
+++ build/tasks/configs/uglify.js
@@ -5,23 +5,34 @@ module.exports = {
 				DEBUG				: false
 			}
 		},
+
 		mangle				: true,
 		beautify			: false,
 		preserveComments	: "some",
 
-		report				: "min",
-		banner				: [
-			"/*!",
-			" * <%= package.name %>",
-			" * @version v<%= package.version %>",
-			" * @date <%= grunt.template.today('yyyy-mm-dd') %>",
-			" * @copyright <%= package.author %>",
-			" */"
-		].join( "\n" )
+		report				: "min"
 	},
+
 	release			: {
+		options			: {
+			banner			: [
+				"/*!",
+				" * <%= package.name %>",
+				" * @version v<%= package.version %>",
+				" * @date <%= grunt.template.today('yyyy-mm-dd') %>",
+				" * @copyright <%= package.author %>",
+				" */"
+			].join( "\n" )
+		},
+
 		// overwrite input file
 		src				: "build/tmp/app/main.js",
 		dest			: "build/tmp/app/main.js"
+	},
+
+	requirejs		: {
+		// overwrite input file
+		src				: "build/tmp/vendor/requirejs/require.js",
+		dest			: "build/tmp/vendor/requirejs/require.js"
 	}
 };
