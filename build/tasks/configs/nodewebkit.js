b1361f89ca106596a22e40badd89c03917bbfe08
bastimeyer <mail@bastimeyer.de>
2014-11-09 23:22:35 +0100

    Temporarily fixed osx builds. resolves #24

--- build/tasks/configs/nodewebkit.js
+++ build/tasks/configs/nodewebkit.js
@@ -5,19 +5,34 @@ module.exports = {
 		version			: "<%= package.config['node-webkit-version'] %>"
 	},
 	win				: {
-		options			: { win: true, winIco: "build/resources/icons/icon-16-32-48-256.ico" },
+		options			: {
+			win				: true,
+			winIco			: "build/resources/icons/icon-16-32-48-256.ico"
+		},
 		src				: "build/tmp/**"
 	},
 	osx				: {
-		options			: { osx: true, macIcns: "build/resources/icons/icon-512.icns" },
+		options			: {
+			osx				: true,
+			macIcns			: "build/resources/icons/icon-512.icns",
+			macPlist		: {
+				LSEnvironment		: {
+					PATH				: "/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+				}
+			}
+		},
 		src				: "build/tmp/**"
 	},
 	linux32			: {
-		options			: { linux32: true },
+		options			: {
+			linux32			: true
+		},
 		src				: "build/tmp/**"
 	},
 	linux64			: {
-		options			: { linux64: true },
+		options			: {
+			linux64			: true
+		},
 		src				: "build/tmp/**"
 	}
 };
