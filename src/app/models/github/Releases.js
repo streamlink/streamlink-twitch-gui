99418c963db7557e0b34aa8266d6b514c78442ca
bastimeyer <mail@bastimeyer.de>
2014-12-07 13:45:48 +0100

    Added some more missing model attributes 2cbc8dd

--- src/app/models/github/Releases.js
+++ src/app/models/github/Releases.js
@@ -1,9 +1,22 @@
 define( [ "ember-data" ], function( DS ) {
 
 	return DS.Model.extend({
-		tag_name: DS.attr( "string" ),
+		assets: DS.attr(),
+		assets_url: DS.attr(),
+		author: DS.attr(),
+		body: DS.attr(),
+		created_at: DS.attr(),
+		draft: DS.attr( "boolean" ),
 		html_url: DS.attr( "string" ),
-		draft: DS.attr( "boolean" )
+		name: DS.attr(),
+		prerelease: DS.attr(),
+		published_at: DS.attr(),
+		tag_name: DS.attr( "string" ),
+		tarball_url: DS.attr(),
+		target_commitish: DS.attr(),
+		upload_url: DS.attr(),
+		url: DS.attr(),
+		zipball_url: DS.attr()
 	}).reopenClass({
 		toString: function() { return "releases"; }
 	});
