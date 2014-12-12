9b791cb15c1d006b7e7ccf57efba86e1081f45cb
bastimeyer <mail@bastimeyer.de>
2014-12-12 14:37:50 +0100

    Improved the has_broadcaster_language logic

--- src/app/models/twitch/Channel.js
+++ src/app/models/twitch/Channel.js
@@ -1,4 +1,7 @@
-define( [ "ember-data" ], function( DS ) {
+define( [ "ember", "ember-data" ], function( Ember, DS ) {
+
+	var	get = Ember.get,
+		re_lang = /^([a-z]{2})(:?-([a-z]{2}))?$/;
 
 	return DS.Model.extend({
 		background: DS.attr( "string" ),
@@ -32,27 +35,33 @@ define( [ "ember-data" ], function( DS ) {
 
 
 		title_followers: function() {
-			var	followers	= this.get( "followers" ),
+			var	followers	= get( this, "followers" ),
 				numerus		= followers === 1 ? "person is" : "people are";
 			return "%@ %@ following".fmt( followers, numerus );
 		}.property( "followers" ),
 
 		title_views: function() {
-			var	views	= this.get( "views" ),
+			var	views	= get( this, "views" ),
 				numerus	= views === 1 ? "view" : "views";
 			return "%@ channel %@".fmt( views, numerus );
 		}.property( "views" ),
 
 
 		has_language: function() {
-			var lang = this.get( "language" );
+			var lang = get( this, "language" );
 			return lang && lang !== "other";
 		}.property( "language" ),
 
 		has_broadcaster_language: function() {
-			var	broadcaster = this.get( "broadcaster_language" ),
-				language = this.get( "language" );
-			return broadcaster && broadcaster !== "other" && broadcaster !== language;
+			var	broadcaster = get( this, "broadcaster_language" ),
+				language = get( this, "language" ),
+				m_broadcaster = re_lang.exec( broadcaster ),
+				m_language = re_lang.exec( language );
+			// show the broadcaster_language only if it is set and
+			// 1. the language is not set or
+			// 2. the language is different from the broadcaster_language
+			//    WITHOUT comparing both lang variants
+			return m_broadcaster && ( !m_language || m_language[1] !== m_broadcaster[1] );
 		}.property( "broadcaster_language", "language" )
 
 	}).reopenClass({
