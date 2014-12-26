c136fbc2537eef40188cf1943ffd0da774005955
bastimeyer <mail@bastimeyer.de>
2014-12-26 13:33:17 +0100

    Fixed missing observer for auth.isLoggedIn
    Also applied indentation changes

--- src/app/models/Auth.js
+++ src/app/models/Auth.js
@@ -2,22 +2,22 @@ define( [ "ember-data" ], function( DS ) {
 
 	return DS.Model.extend({
 		access_token: DS.attr( "string" ),
-		scope: DS.attr( "string" ),
-		date: DS.attr( "date" ),
+		scope       : DS.attr( "string" ),
+		date        : DS.attr( "date" ),
 
 
 		// volatile property
-		user_name: null,
+		user_name : null,
 
 		// status properties
-		isPending: false,
+		isPending : false,
 		isLoggedIn: function() {
-			var	token	= this.get( "access_token" ),
-				name	= this.get( "user_name" ),
-				pending	= this.get( "isPending" );
+			var token   = this.get( "access_token" ),
+			    name    = this.get( "user_name" ),
+			    pending = this.get( "isPending" );
 
 			return token && name && !pending;
-		}.property( "access_token", "user_name" ),
+		}.property( "access_token", "user_name", "isPending" ),
 
 
 		// methods
@@ -25,14 +25,14 @@ define( [ "ember-data" ], function( DS ) {
 		sessionPrepare: function( token, scope ) {
 			this.setProperties({
 				access_token: token,
-				scope: scope,
-				date: new Date()
+				scope       : scope,
+				date        : new Date()
 			});
 		},
 
 		sessionValidate: function( twitchTokenRecord ) {
-			var	valid	= twitchTokenRecord.get( "valid" ),
-				name	= twitchTokenRecord.get( "user_name" );
+			var valid = twitchTokenRecord.get( "valid" ),
+			    name  = twitchTokenRecord.get( "user_name" );
 
 			if ( valid !== true || !name || !name.length ) {
 				throw new Error( "Invalid access token" );
@@ -50,9 +50,9 @@ define( [ "ember-data" ], function( DS ) {
 			// reset all values and save record
 			this.setProperties({
 				access_token: null,
-				scope: null,
-				date: null,
-				user_name: null
+				scope       : null,
+				date        : null,
+				user_name   : null
 			});
 			return this.save();
 		}
