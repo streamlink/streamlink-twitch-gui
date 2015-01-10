39c895b34bccb63b069bfbd29ace1fb85d56bf2c
bastimeyer <mail@bastimeyer.de>
2015-01-10 14:19:48 +0100

    The RESTAdapter now supports dynamic URLs
    Ember-Data is still lacking this feature, so it had to be done in this
    kind of improvised way.
    
    This change enables the implementation of the channel following and
    channel subscription-check mechanics, as well as the listing of channel
    videos and teams the channel belongs to. These kind of requests require
    the URL of the twitch.tv API endpoint to contain the user/channel name.

--- src/app/store/AdapterMixin.js
+++ src/app/store/AdapterMixin.js
@@ -1,7 +1,9 @@
 define( [ "ember" ], function( Ember ) {
 
-	var	get = Ember.get,
-		reURL = /^[a-z]+:\/\/([\w\.]+)\/(.+)$/i;
+	var get   = Ember.get,
+	    push  = [].push,
+	    reURL = /^[a-z]+:\/\/([\w\.]+)\/(.+)$/i;
+
 
 	/**
 	 * Adapter mixin for using static model names
@@ -21,15 +23,60 @@ define( [ "ember" ], function( Ember ) {
 			return this.ajax( this.buildURL( type ), "GET", { data: query } );
 		},
 
-		buildURL: function( type, id ) {
-			var	url	= [ get( this, "host" ) ],
-				ns	= get( this, "namespace" );
+		createRecordMethod: "POST",
+		createRecord: function( store, type, record ) {
+			var id = get( record, "id" );
+			return this.ajax(
+				this.buildURL( type, id ),
+				this.get( "createRecordMethod" ),
+				this.createRecordData( store, type, record )
+			);
+		},
+		createRecordData: function( store, type, record ) {
+			var data = {},
+			    serializer = store.serializerFor( type.typeKey );
+			serializer.serializeIntoHash( data, type, record, { includeId: true } );
+			return { data: data };
+		},
 
-			if (   ns ) { url.push( ns ); }
-			if ( type ) { url.push( type.toString() ); }
-			if (   id ) { url.push( id ); }
+		updateRecordMethod: "PUT",
+		updateRecord: function( store, type, record ) {
+			var id = get( record, "id" );
+			return this.ajax(
+				this.buildURL( type, id ),
+				this.get( "updateRecordMethod" ),
+				this.updateRecordData( store, type, record )
+			);
+		},
+		updateRecordData: function( store, type, record ) {
+			var data = {},
+			    serializer = store.serializerFor( type.typeKey );
+			serializer.serializeIntoHash( data, type, record );
+			return { data: data };
+		},
+
+		deleteRecord: function(store, type, record) {
+			var id = get( record, "id" );
+			return this.ajax( this.buildURL( type, id ), "DELETE" );
+		},
+
+		buildURL: function( type, id ) {
+			var host = get( this, "host" ),
+			    ns   = get( this, "namespace" ),
+			    url  = [ host ];
+
+			// append the adapter specific namespace
+			if (   ns ) { push.call( url, ns ); }
+			// append the type fragments (and process the dynamic ones)
+			if ( type ) { push.apply( url, this.buildURLFragments( type.toString() ) ); }
+			// append the type's ID at the end
+			if (   id ) { push.call( url, id ); }
 
 			return url.join( "/" );
+		},
+
+		buildURLFragments: function( url ) {
+			return url.split( "/" );
 		},
 
 		ajax: function( url, type, options ) {
--- src/app/store/TwitchAdapter.js
+++ src/app/store/TwitchAdapter.js
@@ -4,6 +4,8 @@ define([
 	"store/AdapterMixin"
 ], function( Ember, DS, AdapterMixin ) {
 
+	var reURLFragment = /^:(.+)$/;
+
 	return DS.RESTAdapter.extend( AdapterMixin, {
 		host: "https://api.twitch.tv",
 		namespace: "kraken",
@@ -21,7 +23,37 @@ define([
 			} else {
 				this.headers[ "Authorization" ] = "OAuth " + token;
 			}
-		}.observes( "access_token" )
+		}.observes( "access_token" ),
+
+
+		createRecordMethod: "PUT",
+		createRecordData: function() {
+			// we don't need to send any data with the request (yet?)
+			return {};
+		},
+
+		updateRecordData: function() {
+			// we don't need to send any data with the request (yet?)
+			return {};
+		},
+
+		buildURLFragments: function( url ) {
+			var adapter = this;
+			return this._super( url ).map(function( frag ) {
+				return frag.replace( reURLFragment, function( _, key ) {
+					switch ( key ) {
+						// a user fragment requires the user to be logged in
+						case "user":
+							var user = adapter.get( "auth.user_name" );
+							if ( !user ) { throw new Error( "Unknown user" ); }
+							return user;
+						// unknown fragment
+						default:
+							throw new Error( "Unknown URL fragment: " + key );
+					}
+				});
+			});
+		}
 	});
 
 });
