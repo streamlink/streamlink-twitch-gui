38e01a0d4b9c6e81f16b1d1799b0c27ce3f95879
bastimeyer <mail@bastimeyer.de>
2015-01-12 18:56:40 +0100

    Replaced stream buttons of the watching route
    Replaced the buttons with a single button for opening the stream dialog.

--- src/app/controllers/WatchingController.js
+++ src/app/controllers/WatchingController.js
@@ -1,26 +1,8 @@
 define( [ "ember" ], function( Ember ) {
 
 	return Ember.ArrayController.extend({
-		configBinding: "metadata.package.config",
-
 		sortAscending: false,
-		sortProperties: [ "started" ],
-
-		actions: {
-			"close": function( stream ) {
-				if ( stream && stream.kill ) {
-					stream.kill();
-				}
-			},
-
-			"chat": function( stream ) {
-				this.send(
-					"openBrowser",
-					this.get( "config.twitch-chat-url" )
-						.replace( "{channel}", Ember.get( stream, "name" ) )
-				);
-			}
-		}
+		sortProperties: [ "started" ]
 	});
 
 });
