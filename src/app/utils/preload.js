0a1b3ae48d84c1a1725163a7d9d3f11924191b37
bastimeyer <mail@bastimeyer.de>
2015-03-12 09:53:30 +0100

    Improve/fix image preload utility function

--- src/app/utils/preload.js
+++ src/app/utils/preload.js
@@ -1,40 +1,43 @@
 define( [ "ember" ], function( Ember ) {
 
+	var concat = [].concat,
+	    get = Ember.getWithDefault,
+	    makeArray = Ember.makeArray;
+
 	return function preload( withError, list ) {
 		if ( list === undefined ) {
 			list = withError;
 			withError = false;
 		}
 
-		function createImagePromise( src ) {
-			return new Promise(function promiseImagePreload( resolve, reject ) {
-				var image = new Image();
-				image.addEventListener( "load", resolve, false );
-				image.addEventListener( "error", withError ? reject : resolve, false );
-				image.src = src;
-			});
+		function promiseImage( src ) {
+			var defer = Promise.defer();
+			var image = new Image();
+			image.addEventListener( "load", defer.resolve, false );
+			image.addEventListener( "error", withError ? defer.reject : defer.resolve, false );
+			image.src = src;
+			return defer.promise;
 		}
 
 		return function promisePreload( response ) {
 			// create a new promise containing all image preload promises
 			return Promise.all(
 				// create a flat array out of all traversal strings
-				Ember.makeArray( list ).reduce(function createPromiseList( promises, traverse ) {
-					return [].concat.apply(
-						promises,
-						// traverse response data
-						Ember.getWithDefault( response, traverse, [] )
-							.toArray()
-							// preload images
-							.map( createImagePromise )
-					);
+				makeArray( list ).reduce(function createPromiseList( promises, traverse ) {
+					// traverse response data
+					var resources = get( response, traverse, [] );
+
+					// data instanceof Ember.Enumerable
+					resources = resources.toArray
+						? resources.toArray()
+						: makeArray( resources );
+
+					// preload images
+					return concat.apply( promises, resources.map( promiseImage ) );
 				}, [] )
 			)
 				// return the original response
-				.then(function preloadFulfilled() { return response; })
-				.catch(function() {
-					throw new Error( "Could not load image resources" );
-				});
+				.then(function preloadFulfilled() { return response; });
 		};
 	};
 
