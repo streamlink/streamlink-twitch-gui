b0c5546562a8f038a9bec94df4570ba0c16bbbff
bastimeyer <mail@bastimeyer.de>
2015-04-04 15:33:43 +0200

    Refactor FormButtonComponent

--- src/app/components/FormButtonComponent.js
+++ src/app/components/FormButtonComponent.js
@@ -3,6 +3,9 @@ define([
 	"text!templates/components/formbutton.html.hbs"
 ], function( Ember, template ) {
 
+	var get = Ember.get,
+	    set = Ember.set;
+
 	return Ember.Component.extend({
 		layout: Ember.HTMLBars.compile( template ),
 		tagName: "button",
@@ -18,36 +21,46 @@ define([
 		],
 		class: null,
 
-		action: "",
+		action: null,
 		actionParam: null,
 
 		icon: false,
 		_iconAndText: Ember.computed.and( "icon", "template" ),
 		_iconAndNoText: function() {
-			return this.get( "icon" ) && !this.get( "template" );
+			return get( this, "icon" ) && !get( this, "template" );
 		}.property( "icon", "template" ),
 
 		iconanim: false,
 		_iconanim: false,
 
 		click: function() {
-			var self  = this,
-			    param = Ember.makeArray( this.get( "actionParam" ) ),
-			    success;
-			if ( this.get( "iconanim" ) ) {
-				success = function( callback ) {
-					self.set( "_iconanim", true );
-					self.$().one( "webkitAnimationEnd", function( e ) {
+			var self    = this,
+			    target  = get( this, "targetObject" ),
+			    action  = get( this, "action" ),
+			    context = Ember.makeArray( get( this, "actionParam" ) );
+
+			if ( !action ) { return; }
+
+			if ( get( this, "iconanim" ) ) {
+				context = context.concat(function animationStart( callback ) {
+					set( self, "_iconanim", true );
+					self.$().one( "webkitAnimationEnd", function animationEnd( e ) {
 						e = e.originalEvent;
-						if ( e.animationName === "animIconScale" ) {
-							self.set( "_iconanim", false );
-							if ( callback ) { callback( e ); }
+						if ( e.animationName !== "animIconScale" ) { return; }
+
+						set( self, "_iconanim", false );
+						if ( callback instanceof Function ) {
+							callback( e );
 						}
 					});
-				};
+				});
 			}
 
-			this.sendAction.apply( this, [ "action" ].concat( param ).concat( success ) );
+			this.triggerAction({
+				target: target,
+				action: action,
+				actionContext: context
+			});
 		}
 	});
 
