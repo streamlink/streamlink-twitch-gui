9e0ee177829c9eaae6632920f0e3b868ce646220
bastimeyer <mail@bastimeyer.de>
2015-04-09 22:00:49 +0200

    Move controller logic of Channel to ChannelIndex

--- src/app/controllers/ChannelIndexController.js
+++ src/app/controllers/ChannelIndexController.js
@@ -1,8 +1,54 @@
-define( [ "controllers/ChannelController" ], function( ChannelController ) {
+define([
+	"ember",
+	"controllers/ChannelControllerMixin"
+], function( Ember, ChannelControllerMixin ) {
 
-	return ChannelController.extend({
+	var get = Ember.get;
+
+	return Ember.Controller.extend( ChannelControllerMixin, {
 		needs: [ "channel" ],
-		modelBinding: "controllers.channel.model"
+		model: Ember.computed.alias( "controllers.channel.model" ),
+
+		stream : Ember.computed.alias( "model.stream" ),
+		channel: Ember.computed.alias( "model.channel" ),
+
+
+		_loadSubscriptionAndFollowingData: function() {
+			var channel = get( this, "channel" );
+			this.checkUserSubscribesChannel( channel );
+			this.checkUserFollowsChannel( channel );
+		}.on( "init" ),
+
+		age: function() {
+			return ( new Date() - get( this, "channel.created_at" ) ) / ( 24 * 3600 * 1000 );
+		}.property( "channel.created_at" ),
+
+		previewImage: function() {
+			return get( this, "stream.preview.large_nocache" )
+				|| get( this, "channel.video_banner" );
+		}.property( "stream.preview.large_nocache", "channel.video_banner" ),
+
+		language: function() {
+			var codes = get( this, "config.language_codes" ),
+			    lang  = codes[ get( this, "channel.broadcaster_language" ) ];
+			return lang ? lang[ "lang" ] : "";
+		}.property( "channel.broadcaster_language" ),
+
+		followersPerDay: function() {
+			return get( this, "channel.followers" ) / get( this, "age" );
+		}.property( "age", "channel.followers" ),
+
+		viewsPerDay: function() {
+			return get( this, "channel.views" ) / get( this, "age" );
+		}.property( "age", "channel.views" ),
+
+
+		actions: {
+			"startStream": function( stream ) {
+				if ( !stream ) { return; }
+				this.send( "openLivestreamer", stream );
+			}
+		}
 	});
 
 });
