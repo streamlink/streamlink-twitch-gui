9d2798e322e843b6bb83715873e2b68451002b6d
bastimeyer <mail@bastimeyer.de>
2015-04-13 21:21:44 +0200

    Fix EmbeddedLinksComponent
    Fix "A block may only be used inside an HTML element or another block"
    error caused by "<@twitter>"

--- src/app/components/EmbeddedLinksComponent.js
+++ src/app/components/EmbeddedLinksComponent.js
@@ -1,23 +1,28 @@
 define( [ "ember", "utils/linkmatching" ], function( Ember, linkmatching ) {
 
 	var hbs_string = [
-	    "%@",
-	    "{{#external-link",
-	    " url='%@'",
-	    " targetObject=targetObject}}",
-	    "%@",
-	    "{{/external-link}}"
-	].join( "\n" );
+		"%@",
+		"{{#external-link",
+		" url='%@'",
+		" targetObject=targetObject}}",
+		"%@",
+		"{{/external-link}}"
+	].join( "" );
 
-	var linkurl_re = linkmatching.linkurl_re,
-	    linkurl_fn = linkmatching.linkurl_fn( hbs_string ),
-	    twitter_re = linkmatching.twitter_re,
-	    twitter_fn = linkmatching.twitter_fn( hbs_string );
+	var reSafestringL = /</g;
+	var reSafestringR = />/g;
+
+	var linkurl_re = linkmatching.linkurl_re;
+	var linkurl_fn = linkmatching.linkurl_fn( hbs_string );
+	var twitter_re = linkmatching.twitter_re;
+	var twitter_fn = linkmatching.twitter_fn( hbs_string );
 
 
 	return Ember.Component.extend({
 		layout: function() {
 			var template = String( Ember.getWithDefault( this, "text", "" ) )
+				.replace( reSafestringL, "&lt;" )
+				.replace( reSafestringR, "&gt;" )
 				.replace( linkurl_re, linkurl_fn )
 				.replace( twitter_re, twitter_fn );
 
