7cb5605afa7148837f5ca0a1cf2f6b75758d18eb
bastimeyer <mail@bastimeyer.de>
2015-04-16 21:19:19 +0200

    Don't transform boolean payload props set to null

---
+++ src/app/store/BooleanTransform.js
@@ -0,0 +1,17 @@
+define( [ "ember-data" ], function( DS ) {
+
+	return DS.BooleanTransform.reopen({
+		deserialize: function( serialized ) {
+			return serialized === null
+				? null
+				: this._super( serialized );
+		},
+
+		serialize: function( deserialized ) {
+			return deserialized === null
+				? null
+				: this._super( deserialized );
+		}
+	});
+
+});
