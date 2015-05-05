97bbbb9e5d6788089b455739f434d05ca9782512
bastimeyer <mail@bastimeyer.de>
2015-05-05 09:56:04 +0200

    Refresh routes by pressing F5 or Ctrl+R. Close #85

--- src/app/views/ApplicationView.js
+++ src/app/views/ApplicationView.js
@@ -15,9 +15,16 @@ define([
 		},
 
 		didInsertElement: function() {
-			this._super();
 			guiSelectable();
 			guiSmoothscroll();
+
+			document.documentElement.addEventListener( "keyup", function( e ) {
+				var f5    = e.keyCode === 116;
+				var ctrlR = e.keyCode ===  82 && e.ctrlKey === true;
+				if ( f5 || ctrlR ) {
+					this.get( "controller" ).send( "refresh" );
+				}
+			}.bind( this ), false );
 		}
 	});
 
