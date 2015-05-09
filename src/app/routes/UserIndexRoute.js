streamlink/streamlink-twitch-gui

commit 60dfdcfda1528528f76f3630354956270829b16a

Fix UserIndexRoute redirecting if auth is pending

src/app/routes/UserIndexRoute.js
@@ -32,7 +32,7 @@ define( [ "ember" ], function( Ember ) {
 			}
 
 			// login not pending?
-			if ( !get( auth, "session.isPending" ) ) {
+			if ( get( auth, "session" ) && !get( auth, "session.isPending" ) ) {
 				redirect();
 
 			} else {

commit 8a40ec11caa8b7041cef41b72a3d7bf3143dc969

Fix regression introduced by 60dfdcf

src/app/routes/UserIndexRoute.js
@@ -31,22 +31,30 @@ define( [ "ember" ], function( Ember ) {
 				}
 			}
 
-			// login not pending?
-			if ( get( auth, "session" ) && !get( auth, "session.isPending" ) ) {
-				redirect();
+			function check() {
+				// login not pending?
+				if ( !get( auth, "session.isPending" ) ) {
+					redirect();
+				} else {
+					// show loading screen
+					self.intermediateTransitionTo( "loading" );
 
+					// unregister onLogin callback as soon as the user switches the route
+					// before the callback has fired
+					self.router.one( "didTransition", function() {
+						auth.off( "login", onLogin );
+					});
+
+					// register callback once
+					auth.one( "login", onLogin );
+				}
+			}
+
+			// session record not yet loaded?
+			if ( !get( auth, "session" ) ) {
+				auth.one( "initialized", check );
 			} else {
-				// show loading screen
-				self.intermediateTransitionTo( "loading" );
-
-				// unregister onLogin callback as soon as the user switches the route
-				// before the callback has fired
-				self.router.one( "didTransition", function() {
-					auth.off( "login", self, onLogin );
-				});
-
-				// register callback once
-				auth.one( "login", self, onLogin );
+				check();
 			}
 		}
 	});

src/app/services/AuthService.js
@@ -25,8 +25,8 @@ define([
 	return Ember.Service.extend( Ember.Evented, {
 		metadata: Ember.inject.service(),
 
-		config  : Ember.computed.alias( "metadata.config" ),
-		scope   : Ember.computed.alias( "config.twitch-oauth-scope" ),
+		config: Ember.computed.alias( "metadata.config" ),
+		scope : Ember.computed.alias( "config.twitch-oauth-scope" ),
 
 		session: null,
 
@@ -63,6 +63,9 @@ define([
 					var token = get( session, "access_token" );
 					self.login( token, true )
 						.catch(function() {});
+
+					// trigger event after calling login, so `isPending` can be set first
+					self.trigger( "initialized" );
 				});
 		},
 