385654af06170d4c5b1514cb81a27f29cd8af2b3
bastimeyer <mail@bastimeyer.de>
2015-05-18 18:00:45 +0200

    Fix UserSubscriptionSerializer

--- src/app/models/twitch/UserSubscriptionSerializer.js
+++ src/app/models/twitch/UserSubscriptionSerializer.js
@@ -1,29 +1,16 @@
 define( [ "store/TwitchSerializer" ], function( TwitchSerializer ) {
 
 	return TwitchSerializer.extend({
-		primaryKey: "id",
-
-		attrs: {
-			channel: { deserialize: "records" }
-		},
-
 		typeForRoot: function() {
-			return "twitchUserSubscriptions";
+			return "twitchUserSubscription";
 		},
 
 		normalizePayload: function( payload ) {
-			// expected payload
-			if ( payload.follows ) { return payload; }
-			// unexpected payload: normalize it! also don't forget to set a proper ID
-			if ( !payload.id ) { payload.id = payload.channel.name; }
-			return { follows: payload };
-		},
-
-		normalizeHash: {
-			follows: function( hash ) {
-				if ( !hash.id ) { hash.id = hash.channel.name; }
-				return hash;
-			}
+			payload._id = payload.channel.name;
+			payload.channel = null;
+			return {
+				twitchUserSubscription: payload
+			};
 		}
 	});
 
