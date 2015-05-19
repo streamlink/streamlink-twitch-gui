4142d6201d7e5739305ece6852764e822f3116ec
bastimeyer <mail@bastimeyer.de>
2015-05-19 23:25:32 +0200

    Fix channel page refreshing bug. Resolves #89

--- src/app/routes/ChannelRoute.js
+++ src/app/routes/ChannelRoute.js
@@ -15,6 +15,11 @@ define( [ "ember", "utils/preload" ], function( Ember, preload ) {
 						channel: get( stream, "channel" )
 					};
 				}, function() {
+					// let the stream record transition from root.loading into root.empty
+					// so that it can be reloaded later on... fixes #89
+					var stream = store.recordForId( "twitchStream", id );
+					stream.notFound();
+
 					// if the channel is not online, just *fetch* the channel record
 					return store.fetchById( "twitchChannel", id )
 						.then(function( channel ) {
