streamlink/streamlink-twitch-gui

commit 6bda7f66f3d2b871983c4513f2c25219ec505e69

Add cascading animation effect to list items
* Let StreamItemComponent, GameItemComponent, ChannelItemComponent and
  SubscriptionItemComponent inherit from ListItemComponent.
* Add initialFetchSize property to InfiniteScroll controllers.

src/app/components/SubscriptionItemComponent.js
@@ -1,21 +1,20 @@
 define([
 	"ember",
-	"mixins/PreviewImageViewMixin",
+	"components/ListItemComponent",
 	"moment",
 	"text!templates/components/subscription.html.hbs"
-], function( Ember, PreviewImageViewMixin, moment, template ) {
+], function( Ember, ListItemComponent, moment, template ) {
 
 	var get = Ember.get;
 
-	return Ember.Component.extend( PreviewImageViewMixin, {
+	return ListItemComponent.extend({
 		metadata: Ember.inject.service(),
 
 		layout: Ember.HTMLBars.compile( template ),
-		tagName: "li",
-		classNameBindings: [ ":subscription-component" ],
+		classNames: [ "subscription-component" ],
 		attributeBindings: [ "style" ],
 
-		product  : Ember.computed.alias( "subscription.product" ),
+		product  : Ember.computed.alias( "content.product" ),
 		channel  : Ember.computed.alias( "product.partner_login" ),
 		emoticons: Ember.computed.alias( "product.emoticons" ),
 

src/app/components/ChannelItemComponent.js
@@ -1,12 +1,11 @@
 define([
 	"ember",
-	"mixins/PreviewImageViewMixin",
+	"components/ListItemComponent",
 	"text!templates/components/channel.html.hbs"
-], function( Ember, PreviewImageViewMixin, template ) {
+], function( Ember, ListItemComponent, template ) {
 
-	return Ember.Component.extend( PreviewImageViewMixin, {
+	return ListItemComponent.extend({
 		layout: Ember.HTMLBars.compile( template ),
-		tagName: "li",
 		classNames: [ "channel-component" ]
 	});
 

src/app/components/GameItemComponent.js
@@ -1,20 +1,22 @@
 define([
 	"ember",
-	"mixins/PreviewImageViewMixin",
+	"components/ListItemComponent",
 	"text!templates/components/game.html.hbs"
-], function( Ember, PreviewImageViewMixin, template ) {
+], function( Ember, ListItemComponent, template ) {
 
-	return Ember.Component.extend( PreviewImageViewMixin, {
+	var get = Ember.get;
+
+	return ListItemComponent.extend({
 		layout: Ember.HTMLBars.compile( template ),
-		tagName: "li",
 		classNames: [ "game-component" ],
 
 		action: "goto",
 
-		hasStats: Ember.computed.any( "channels", "viewers" ),
+		game: Ember.computed.any( "content.game", "content" ),
+		hasStats: Ember.computed.any( "content.channels", "content.viewers" ),
 
 		click: function() {
-			this.sendAction( "action", "games.game", this.get( "game.name" ) );
+			this.sendAction( "action", "games.game", get( this, "game.name" ) );
 		}
 	});
 

src/app/components/ListItemComponent.js
@@ -1,0 +1,13 @@
+define([
+	"ember",
+	"mixins/PreviewImageViewMixin"
+], function( Ember, PreviewImageViewMixin ) {
+
+	return Ember.Component.extend( PreviewImageViewMixin, {
+		tagName: "li",
+		classNameBindings: [ "isNewItem:newItem" ],
+
+		isNewItem: false
+	});
+
+});

src/app/components/StreamItemComponent.js
@@ -1,22 +1,25 @@
 define([
 	"ember",
-	"mixins/PreviewImageViewMixin",
+	"components/ListItemComponent",
 	"text!templates/components/stream.html.hbs"
-], function( Ember, PreviewImageViewMixin, template ) {
+], function( Ember, ListItemComponent, template ) {
 
-	return Ember.Component.extend( PreviewImageViewMixin, {
+	var get = Ember.get;
+
+	return ListItemComponent.extend({
 		layout: Ember.HTMLBars.compile( template ),
-		tagName: "li",
 		classNameBindings: [ ":stream-component", "_showGame:show-game" ],
 
 		action: "openLivestreamer",
 
+		channel: Ember.computed.alias( "content.channel" ),
+
 		showGame: false,
-		_showGame: Ember.computed.and( "showGame", "stream.channel.game" ),
+		_showGame: Ember.computed.and( "showGame", "channel.game" ),
 
 		actions: {
 			"startStream": function() {
-				this.sendAction( "action", this.get( "stream" ) );
+				this.sendAction( "action", get( this, "content" ) );
 			}
 		}
 	});

src/app/mixins/InfiniteScrollRouteMixin.js
@@ -123,7 +123,7 @@ define( [ "ember" ], function( Ember ) {
 			this.calcFetchSize();
 		},
 
-		setupController: function( controller ) {
+		setupController: function( controller, model ) {
 			this._super.apply( this, arguments );
 
 			// late bindings
@@ -141,6 +141,7 @@ define( [ "ember" ], function( Ember ) {
 
 			set( controller, "isFetching", false );
 			set( controller, "hasFetchedAll", offset < limit );
+			set( controller, "initialFetchSize", model.length );
 		},
 
 		fetchContent: function() {

src/styles/animations.less
@@ -45,6 +45,18 @@
 	}
 }
 
+@-webkit-keyframes animFadeInBottom {
+	from {
+		opacity: 0;
+		transform: translateY( 3rem );
+	}
+
+	to {
+		opacity: 1;
+		transform: translateY( 0 );
+	}
+}
+
 @-webkit-keyframes animIconScale {
 	0% {
 		opacity: 1;

src/styles/config.less
@@ -115,3 +115,7 @@
 
 @anim-sidebar-speed: .2s;
 @anim-sidebar-easing: ease-out;
+@anim-content-duration: .333s;
+@anim-list-item-duration: .333s;
+@anim-list-item-offset-min: ( @anim-content-duration * 1 / 2 );
+@anim-list-item-offset-max: 1s;

commit c975acf4cf592080aef03effc033dd85d8f75407

Fix expiration time of SubscriptionItemComponent
Regression introduced in 6bda7f6

src/app/components/SubscriptionItemComponent.js
@@ -24,14 +24,14 @@ define([
 		}.property( "channel.profile_banner" ),
 
 		hasEnded: function() {
-			var access_end = get( this, "subscription.access_end" );
+			var access_end = get( this, "content.access_end" );
 			return new Date() > access_end;
-		}.property( "subscription.access_end" ),
+		}.property( "content.access_end" ).volatile(),
 
 		ends: function() {
-			var access_end = get( this, "subscription.access_end" );
+			var access_end = get( this, "content.access_end" );
 			return moment().to( access_end );
-		}.property( "subscription.access_end" ).volatile(),
+		}.property( "content.access_end" ).volatile(),
 
 
 		buttonAction: "openBrowser",