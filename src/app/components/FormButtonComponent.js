18cc52c817fd3945accfdc5b6cf674e74146885e
bastimeyer <mail@bastimeyer.de>
2015-09-04 22:21:57 +0200

    Work around the hasBlock restriction of components
    Component classes can't be set based on the `hasBlock` property,
    because it is only available inside the component's template. Use an
    empty component tagName and move the button into the template.
    Fixes FormButtonComponent...

--- src/app/components/FormButtonComponent.js
+++ src/app/components/FormButtonComponent.js
@@ -5,68 +5,64 @@ define([
 
 	var get = Ember.get;
 	var set = Ember.set;
-	var and = Ember.computed.and;
+	var $ = Ember.$;
 
 	return Ember.Component.extend({
 		layout: Ember.HTMLBars.compile( template ),
-		tagName: "button",
-		attributeBindings: [ "type", "title", "disabled" ],
-		type: "button",
-		title: null,
-		classNameBindings: [
-			":btn",
-			"_iconAndNoText:btn-icon",
-			"_iconAndText:btn-with-icon",
-			"_iconanim:btn-with-anim",
-			"class"
-		],
+
+		tagName: "",
+
+		title  : null,
 		"class": null,
 
 		action     : null,
 		actionParam: null,
 
-		icon: false,
-		_iconAndText: and( "icon", "hasBlock" ),
-		_iconAndNoText: function() {
-			return get( this, "icon" ) && !get( this, "hasBlock" );
-		}.property( "icon", "hasBlock" ),
+		icon    : false,
+		iconanim: false,
 
-		iconanim : false,
 		_iconanim: false,
 
-		click: function() {
-			var action  = get( this, "action" );
-			if ( !action ) { return; }
+		_iconAnimation: function( data ) {
+			var self  = this;
+			var defer = Promise.defer();
+			// dirty
+			var element = this._renderNode.firstNode;
 
-			var self    = this;
-			var context = Ember.makeArray( get( this, "actionParam" ) );
+			set( self, "_iconanim", true );
+			$( element ).one( "webkitAnimationEnd", function( e ) {
+				if ( e.originalEvent.animationName !== "animIconScale" ) { return; }
 
-			if ( get( this, "iconanim" ) ) {
-				context = context.concat(function animationStart( data ) {
-					var defer = Promise.defer();
-					set( self, "_iconanim", true );
-					self.$().one( "webkitAnimationEnd", function animationEnd( e ) {
-						e = e.originalEvent;
-						if ( e.animationName !== "animIconScale" ) { return; }
+				set( self, "_iconanim", false );
+				defer.resolve( data );
+			});
 
-						set( self, "_iconanim", false );
-						defer.resolve( data );
+			return defer.promise;
+		},
+
+		actions: {
+			click: function() {
+				var action  = get( this, "action" );
+				if ( !action ) { return; }
+
+				var context = Ember.makeArray( get( this, "actionParam" ) );
+
+				if ( this.attrs.iconanim ) {
+					context.push( this._iconAnimation.bind( this ) );
+				}
+
+				// allow the component to send actions to itself
+				// in case it has been extended and uses its own actions
+				if ( this._actions instanceof Object && this._actions.hasOwnProperty( action ) ) {
+					this.send.apply( this, [ action ].concat( context ) );
+
+				} else {
+					this.triggerAction({
+						target: get( this, "targetObject" ),
+						action: action,
+						actionContext: context
 					});
-					return defer.promise;
-				});
-			}
-
-			// allow the component to send actions to itself
-			// in case it has been extended and uses its own actions
-			if ( this._actions instanceof Object && this._actions.hasOwnProperty( action ) ) {
-				this.send.apply( this, [ action ].concat( context ) );
-
-			} else {
-				this.triggerAction({
-					target: get( this, "targetObject" ),
-					action: action,
-					actionContext: context
-				});
+				}
 			}
 		}
 	});
