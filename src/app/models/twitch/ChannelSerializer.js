streamlink/streamlink-twitch-gui

commit d5ee6ed195a39c30e42d0a62febc3259819976a9

Remove twitchTeam model and references
as it is not used right now.
This commit may be reverted once team pages or anything team related is
being implemented.

src/app/models/twitch/ChannelSerializer.js
@@ -26,6 +26,9 @@ define( [ "store/TwitchSerializer" ], function( TwitchSerializer ) {
 			if ( resourceHash ) {
 				delete resourceHash.bio;
 				delete resourceHash.type;
+
+				// also remove the teams property for now
+				delete resourceHash.teams;
 			}
 
 			return this._super( modelClass, resourceHash, prop );

src/app/app.js
@@ -62,8 +62,6 @@ define(function( require ) {
 		TwitchStreamSerializer: require( "models/twitch/StreamSerializer" ),
 		TwitchChannel: require( "models/twitch/Channel" ),
 		TwitchChannelSerializer: require( "models/twitch/ChannelSerializer" ),
-		TwitchTeam: require( "models/twitch/Team" ),
-		TwitchTeamSerializer: require( "models/twitch/TeamSerializer" ),
 		TwitchImage: require( "models/twitch/Image" ),
 		TwitchImageSerializer: require( "models/twitch/ImageSerializer" ),
 

src/app/models/twitch/Channel.js
@@ -2,7 +2,6 @@ define( [ "Ember", "EmberData" ], function( Ember, DS ) {
 
 	var get = Ember.get;
 	var attr = DS.attr;
-	var hasMany = DS.hasMany;
 
 	var reLang = /^([a-z]{2})(:?-([a-z]{2}))?$/;
 
@@ -25,7 +24,7 @@ define( [ "Ember", "EmberData" ], function( Ember, DS ) {
 		profile_banner_background_color: attr( "string" ),
 		staff: attr( "boolean" ),
 		status: attr( "string" ),
-		teams: hasMany( "twitchTeam" ),
+		//teams: hasMany( "twitchTeam" ),
 		updated_at: attr( "date" ),
 		url: attr( "string" ),
 		video_banner: attr( "string" ),

src/app/models/twitch/Team.js
@@ -1,16 +1,0 @@
-define( [ "EmberData" ], function( DS ) {
-
-	var attr = DS.attr;
-
-	return DS.Model.extend({
-		background: attr( "string" ),
-		banner: attr( "string" ),
-		created_at: attr( "date" ),
-		display_name: attr( "string" ),
-		info: attr( "string" ),
-		logo: attr( "string" ),
-		name: attr( "string" ),
-		updated_at: attr( "date" )
-	});
-
-});

src/app/models/twitch/TeamSerializer.js
@@ -1,9 +1,0 @@
-define( [ "store/TwitchSerializer" ], function( TwitchSerializer ) {
-
-	return TwitchSerializer.extend({
-		modelNameFromPayloadKey: function() {
-			return "twitchTeam";
-		}
-	});
-
-});

commit 20369d3a14975590ca6939b86c63f89f49fc1670

Fix warning message caused by d5ee6ed

src/app/models/twitch/ChannelSerializer.js
@@ -7,10 +7,6 @@ define( [ "store/TwitchSerializer" ], function( TwitchSerializer ) {
 			return "twitchChannel";
 		},
 
-		attrs : {
-			teams: { deserialize: "records" }
-		},
-
 		normalizeResponse: function( store, primaryModelClass, payload, id, requestType ) {
 			payload = {
 				twitchChannel: payload