streamlink/streamlink-twitch-gui

commit 547fac0bed0dfb85357b9057ce523c8a451893d3

Use Masonry library and add ChannelPanelsComponent

src/app/components/ChannelPanelsComponent.js
@@ -1,0 +1,31 @@
+define([
+	"Ember",
+	"Masonry",
+	"hbs!templates/components/ChannelPanelsComponent"
+], function(
+	Ember,
+	Masonry,
+	layout
+) {
+
+	var run = Ember.run;
+	var scheduleOnce = run.scheduleOnce;
+
+	return Ember.Component.extend({
+		layout: layout,
+		tagName: "section",
+		classNames: [ "content-panels" ],
+
+		_masonry: function() {
+			var container = this.$( "ul" )[0];
+			scheduleOnce( "afterRender", function() {
+				return new Masonry( container, {
+					itemSelector: ".panel-item-component",
+					columnWidth: ".panel-item-component",
+					transitionDuration: 0
+				});
+			});
+		}.on( "didInsertElement" )
+	});
+
+});

src/app/app.js
@@ -203,6 +203,7 @@ define(function( require ) {
 		ModalLogComponent: require( "components/ModalLogComponent" ),
 		LoadingSpinnerComponent: require( "components/LoadingSpinnerComponent" ),
 		HeadlineTotalsComponent: require( "components/HeadlineTotalsComponent" ),
+		ChannelPanelsComponent: require( "components/ChannelPanelsComponent" ),
 		PanelItemComponent: require( "components/PanelItemComponent" ),
 
 

src/app/config.js
@@ -41,6 +41,7 @@ requirejs.config({
 		"JQuery"       : "../vendor/jquery/dist/jquery",
 		"Selecter"     : "../vendor/Selecter/jquery.fs.selecter",
 		"Moment"       : "../vendor/momentjs/moment",
+		"Masonry"      : "../vendor/masonry/dist/masonry.pkgd",
 
 		// Wrappers
 		"EmberHtmlbarsWrapper": "../requirejs/wrappers/EmberHtmlbarsWrapper",

src/styles/components/PanelItemComponent.less
@@ -1,11 +1,11 @@
 
 .panel-item-component {
-	float: left;
+	// width in pixels
+	width: 320px;
 	margin: 0 3.125em 2em 0;
 
 	> * {
-		// width in pixels
-		max-width: 320px;
+		max-width: 100%;
 		margin: 0 auto;
 	}
 

src/styles/content/list.less
@@ -1,7 +1,6 @@
 
-.content-list {
-	.clearfix();
-
+.content-list,
+.content-list-grid {
 	margin: 0;
 	padding: 0;
 	list-style: none;
@@ -16,8 +15,6 @@
 	});
 
 	> li {
-		float: left;
-
 		&:not(.newItem) {
 			@min: @anim-list-item-offset-min;
 			@max: @anim-list-item-offset-max;
@@ -46,5 +43,13 @@
 			}
 			.loop( 1 );
 		}
+	}
+}
+
+.content-list {
+	.clearfix();
+
+	> li {
+		float: left;
 	}
 }

src/templates/channel/ChannelIndex.hbs
@@ -40,12 +40,5 @@
 </div>
 </section>
 {{#if panels.length}}
-<section class="content-panels">
-<h3>Channel details</h3>
-<ul class="content-list">
-{{#each panels as |panel|}}
-{{panel-item panel=panel}}
-{{/each}}
-</ul>
-</section>
+{{channel-panels panels=panels}}
 {{/if}}

src/templates/components/ChannelPanelsComponent.hbs
@@ -1,0 +1,6 @@
+<h3>Channel details</h3>
+<ul class="content-list-grid">
+{{#each panels as |panel|}}
+{{panel-item panel=panel}}
+{{/each}}
+</ul>

commit 6c6f4949b661cbd04ed2dc3be9c693324a190925

Fix links in PanelItemComponent
caused by 547fac0

src/app/components/ChannelPanelsComponent.js
@@ -16,6 +16,8 @@ define([
 		tagName: "section",
 		classNames: [ "content-panels" ],
 
+		action: "openBrowser",
+
 		_masonry: function() {
 			var container = this.$( "ul" )[0];
 			scheduleOnce( "afterRender", function() {
@@ -25,7 +27,13 @@ define([
 					transitionDuration: 0
 				});
 			});
-		}.on( "didInsertElement" )
+		}.on( "didInsertElement" ),
+
+		actions: {
+			openBrowser: function( url ) {
+				this.sendAction( "action", url );
+			}
+		}
 	});
 
 });