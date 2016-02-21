4a81db5789f06584297d4146cbe8c70cf7af7623
bastimeyer <mail@bastimeyer.de>
2016-02-21 17:46:11 +0100

    Fix open dialog button in WatchingRoute

--- src/app/controllers/WatchingController.js
+++ src/app/controllers/WatchingController.js
@@ -1,12 +1,20 @@
 define( [ "Ember" ], function( Ember ) {
 
+	var get = Ember.get;
 	var sort = Ember.computed.sort;
 
 	return Ember.Controller.extend({
 		auth: Ember.inject.service(),
+		livestreamer: Ember.inject.service(),
 
 		sortedModel: sort( "model", "sortBy" ),
-		sortBy: [ "started:desc" ]
+		sortBy: [ "started:desc" ],
+
+		actions: {
+			"openLivestreamer": function( stream ) {
+				get( this, "livestreamer" ).startStream( stream );
+			}
+		}
 	});
 
 });
