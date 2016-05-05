182e32981e0d58b115a0e8d1a8502179606827d3
bastimeyer <mail@bastimeyer.de>
2016-05-05 18:02:38 +0200

    Add a preferences item to the OSX menu bar
    Resolve #245

--- src/app/nwjs/setup/menubar.js
+++ src/app/nwjs/setup/menubar.js
@@ -1,31 +1,49 @@
 define([
+	"nwjs/nwGui",
 	"nwjs/nwWindow",
 	"nwjs/menu",
 	"utils/node/platform",
 	"json!root/metadata"
 ], function(
+	nwGui,
 	nwWindow,
 	Menu,
 	platform,
 	metadata
 ) {
 
+	var MenuItem = nwGui.MenuItem;
 	var isDarwin = platform.isDarwin;
 
 	var displayName = metadata.package.config[ "display-name" ];
 
-	function createNativeMenuBarDarwin() {
+
+	function createNativeMenuBarDarwin( controller ) {
 		var menubar = Menu.create({ type: "menubar" });
 		menubar.createMacBuiltin( displayName );
-		return menubar.menu;
+		menubar = menubar.menu;
+
+		// Add a preferences menu item to the main menubar submenu
+		var mainMenu = menubar.items[ 0 ].submenu;
+		mainMenu.insert( new MenuItem({ type: "separator" }), 1 );
+		var preferences = new MenuItem({
+			type: "normal",
+			label: "Preferences",
+			key: ",",
+			modifier: "cmd"
+		});
+		preferences.click = controller.send.bind( controller, "goto", "settings" );
+		mainMenu.insert( preferences, 2 );
+
+		return menubar;
 	}
 
 
 	return {
-		createNativeMenuBar: function() {
+		createNativeMenuBar: function( controller ) {
 			if ( !isDarwin ) { return; }
 
-			nwWindow.menu = createNativeMenuBarDarwin();
+			nwWindow.menu = createNativeMenuBarDarwin( controller );
 		}
 	};
 
