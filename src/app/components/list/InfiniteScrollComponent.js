63c7dc9fe61d1ec4a0cd2327cfb13cc587935ef6
bastimeyer <mail@bastimeyer.de>
2016-05-24 00:07:54 +0200

    Fix parent elem logic in InfiniteScrollComponent
    See 8a8e661

--- src/app/components/list/InfiniteScrollComponent.js
+++ src/app/components/list/InfiniteScrollComponent.js
@@ -11,7 +11,12 @@ define([
 	var alias = Ember.computed.alias;
 	var or = Ember.computed.or;
 
-	var $window = Ember.$( window );
+	var $ = Ember.$;
+	var $window = $( window );
+
+	var document = window.document;
+	var documentElement = document.documentElement;
+	var getComputedStyle = window.getComputedStyle;
 
 
 	return Ember.Component.extend({
@@ -45,29 +50,49 @@ define([
 
 
 		didInsertElement: function() {
-			this._super();
+			this._super.apply( this, arguments );
 
-			var $elem     = this.$().parent();
+			// find first parent node which has a scroll bar
+			var overflow;
+			var parent = get( this, "element" );
+			while ( ( parent = parent.parentNode ) ) {
+				if ( parent.clientHeight >= parent.scrollHeight ) {
+					continue;
+				}
+				overflow = getComputedStyle( parent, "" ).getPropertyValue( "overflow-y" );
+				if ( overflow === "scroll" || overflow === "auto" ) {
+					break;
+				}
+			}
+			// fallback: use the main content element or the document body
+			if ( parent === documentElement ) {
+				parent = documentElement.querySelector( "main.content" ) || document.body;
+			}
+
+			var $parent   = $( parent );
 			var threshold = get( this, "scrollThreshold" );
 			var target    = get( this, "targetObject" );
-			var listener  = this.infiniteScroll.bind( this, $elem[ 0 ], threshold, target );
+			var listener  = this.infiniteScroll.bind( this, parent, threshold, target );
 
+			set( this, "$parent", $parent );
 			set( this, "scrollListener", listener );
 
-			$elem.on( "scroll", listener );
+			$parent.on( "scroll", listener );
 			$window.on( "resize", listener );
 		},
 
+
 		willDestroyElement: function() {
-			this._super();
+			this._super.apply( this, arguments );
 
+			var $parent = get( this, "$parent" );
 			var scrollListener = get( this, "scrollListener" );
-			var $elem = this.$();
-			$elem.parent().off( "scroll", scrollListener );
+			$parent.off( "scroll", scrollListener );
 			$window.off( "resize", scrollListener );
-
+			set( this, "$parent", null );
 			set( this, "scrollListener", null );
 		},
+
 
 		infiniteScroll: function( elem, percentage, target ) {
 			var threshold = percentage * elem.clientHeight;
