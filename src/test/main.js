558576c4ca59dfd4282c146e1a03cfe53718d7b8
bastimeyer <mail@bastimeyer.de>
2016-11-22 07:35:08 +0100

    Remove babel-polyfill dependency
    We're running tests in NW.js now and don't need polyfills anymore

--- src/test/main.js
+++ src/test/main.js
@@ -1,4 +1,3 @@
-import {} from "Shim";
 import QUnit, {
 	config
 } from "QUnit";
--- src/test/web_modules/Shim.js
+++
@@ -1,4 +0,0 @@
-// explicitly import needed shims from "babel-polyfill"
-import "core-js/modules/es6.number.max-safe-integer";
-import "core-js/modules/es6.number.min-safe-integer";
-import "core-js/modules/es6.number.is-integer";
