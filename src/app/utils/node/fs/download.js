8b79ff7517df225d25efd5da551213ce632f8a31
bastimeyer <mail@bastimeyer.de>
2016-11-26 05:50:39 +0100

    Rewrite download module
    Support redirects

--- src/app/utils/node/fs/download.js
+++ src/app/utils/node/fs/download.js
@@ -3,55 +3,79 @@ import {
 	isDirectory
 } from "utils/node/fs/stat";
 import mkdirp from "utils/node/fs/mkdirp";
-import PATH from "path";
-import FS from "fs";
-import HTTP from "http";
-import HTTPS from "https";
-
-
-const reScheme = /^(?:http(s)?):\/\/(?:.*\/)+([^\/]+)?$/i;
+import getRedirected from "utils/node/http/getRedirected";
+import {
+	basename,
+	resolve as pathResolve
+} from "path";
+import { WriteStream } from "fs";
+import { parse as urlParse } from "url";
 
 
 /**
- * Download a file via HTTP/HTTPS and save it to the filesystem
- * @param {String} url
- * @param {String} dir
- * @param {String?} name
+ * @param {Url} url
+ * @param {(Writable|EventEmitter)} writeStream
  * @returns {Promise}
  */
-function download( url, dir, name ) {
-	let match = reScheme.exec( url );
-	if ( !dir || !match || !match[2] ) { return Promise.reject(); }
+function downloadPromise( url, writeStream ) {
+	return getRedirected( url )
+		.then( incomingMessage =>
+			new Promise( ( resolve, reject ) => {
+				writeStream.once( "error", reject );
 
-	// if name is not set, use the remote path as file name
-	let dest = PATH.join( dir, name || match[2] || "download" );
+				incomingMessage.once( "end", resolve );
+				incomingMessage.once( "error", reject );
+				incomingMessage.once( "unpipe", () => reject( new Error( "I/O error" ) ) );
 
-	// check if the file already exists
-	return stat( dest )
-		// file does not exist
-		.catch(function() {
-			// check if the directory exists
-			return stat( dir, isDirectory )
-				// try to create directory
-				.catch( mkdirp.bind( null, dir ) )
-				// now start the download
-				.then(function() {
-					return new Promise(function ( resolve, reject ) {
-						let write = FS.createWriteStream( dest );
+				// link readStream and writeStream
+				incomingMessage.pipe( writeStream, { end: true } );
+			})
+				.finally( () => {
+					incomingMessage.destroy();
+					writeStream.end();
+				})
+		);
+}
 
-						write.on( "error", function() {
-							write.close( reject );
-						});
-						write.on( "finish", function() {
-							write.close( resolve.bind( null, dest ) );
-						});
 
-						// start the download via https or http depending on the url scheme
-						( match[1] ? HTTPS : HTTP ).get( url, function( response ) {
-							response.pipe( write );
-						}).on( "error", reject );
-					});
-				});
+/**
+ * Download a file over http(s) and write it to the filesystem
+ * @param {(String|Url)} url
+ * @param {(String|Object)} dest
+ * @param {String?} dest.directory
+ * @param {String?} dest.name
+ * @returns {Promise}
+ */
+function download( url, dest ) {
+	if ( typeof url === "string" ) {
+		url = urlParse( url );
+	}
+	if ( !url.protocol || !url.host ) {
+		return Promise.reject( new Error( "Invalid download URL" ) );
+	}
+
+	if ( typeof dest === "string" ) {
+		dest = {
+			dir : dest,
+			name: null
+		};
+	}
+	if ( !dest || !dest.dir || typeof dest.dir !== "string" ) {
+		return Promise.reject( new Error( "Invalid directory" ) );
+	}
+
+	// does the path exist?
+	return stat( dest.dir, isDirectory )
+		// if not, try to create it
+		.catch( () => mkdirp( dest.dir ) )
+		// download and save
+		.then( () => {
+			let name   = dest.name || basename( url.pathname ) || "download";
+			let path   = pathResolve( dest.dir, name );
+			let stream = new WriteStream( path );
+
+			return downloadPromise( url, stream )
+				.then( () => path );
 		});
 }
 
---
+++ src/app/utils/node/http/getRedirected.js
@@ -0,0 +1,92 @@
+import { get as httpGet } from "http";
+import { get as httpsGet } from "https";
+import {
+	parse as urlParse,
+	format as urlFormat
+} from "url";
+
+
+const MAX_REDIRECTS = 3;
+
+
+class Redirection {
+	constructor( dest ) {
+		if ( !dest ) {
+			throw new Error( "Missing location header" );
+		}
+
+		this.dest = dest;
+	}
+}
+
+
+function mergeURLs( a, b ) {
+	let parsedB = urlParse( a );
+	// do nothing if second URL contains host
+	if ( parsedB.host ) { return b; }
+
+	// take first URL's protocol and host and apply it to the second one
+	let parsedA = urlParse( a );
+
+	parsedB.protocol = parsedA.protocol;
+	parsedB.auth = parsedA.auth;
+	parsedB.host = parsedA.host;
+
+	return urlFormat( parsedB );
+}
+
+function promiseRequest( url ) {
+	return new Promise( ( resolve, reject ) => {
+		// detect protocol
+		const get = urlParse( url ).protocol === "https:"
+			? httpsGet
+			: httpGet;
+
+		// send a GET request
+		get( url, res => {
+			let status = res.statusCode;
+
+			if ( status >= 300 && status < 400 ) {
+				// redirection
+				reject( new Redirection( res.headers.location ) );
+
+			} else if ( status !== 200 ) {
+				// error
+				reject( new Error( "HTTP Error: " + status ) );
+
+			} else {
+				// success
+				resolve( res );
+			}
+		})
+			.on( "error", reject );
+	});
+}
+
+function doRequest( url, num ) {
+	return promiseRequest( url )
+		.catch( data => {
+			if ( !( data instanceof Redirection ) ) {
+				// error
+				return Promise.reject( data );
+
+			} else if ( num > MAX_REDIRECTS ) {
+				// too many redirects
+				return Promise.reject( new Error( "Maximum number of redirects reached" ) );
+
+			} else {
+				// add current host to url if it's a relative one
+				url = mergeURLs( url, data.dest );
+				// try again with new url
+				return doRequest( url, ++num );
+			}
+		});
+}
+
+
+function getRedirected( url ) {
+	return doRequest( url, 1 );
+}
+
+
+export default getRedirected;
