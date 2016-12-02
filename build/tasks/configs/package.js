0d7027fdd890e451993c93057341c64f428aeaa7
bastimeyer <mail@bastimeyer.de>
2016-12-02 06:53:28 +0100

    Change all app internal URLs

--- build/tasks/configs/package.js
+++ build/tasks/configs/package.js
@@ -5,8 +5,8 @@ module.exports = {
 	options: {
 		changelogFile: PATH.resolve( "CHANGELOG.md" ),
 		// has a rate limit of 60 requests per IP per hour
-		githubReleases: "https://api.github.com/repos/bastimeyer/livestreamer-twitch-gui/releases",
-		githubReleasesAtom: "https://github.com/bastimeyer/livestreamer-twitch-gui/releases.atom"
+		githubReleases: "https://api.github.com/repos/streamlink/streamlink-twitch-gui/releases",
+		githubReleasesAtom: "https://github.com/streamlink/streamlink-twitch-gui/releases.atom"
 	},
 
 	chocolatey: {
