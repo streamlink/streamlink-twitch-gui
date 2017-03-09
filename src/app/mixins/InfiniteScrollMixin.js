streamlink/streamlink-twitch-gui

commit e74ef352ba542987dc9efb5b3bc875b2740a74f9

Remove min-width constraint from first media query
in .dynamic-elems-per-row() mixin.

Resolves #411

src/styles/mixins.less
@@ -129,15 +129,22 @@
 			&:nth-of-type( @{nth} ) { margin-right: 0; }
 		}
 
+		// the first media query should only have a max-width constraint
+		.mediaquery() when ( @i = @min ) {
+			@media ( max-width: @max-width ) {
+				.rules();
+			}
+		}
+
 		// max-width constraint required, so the nth-child rule doesn't get used by other queries
-		.mediaquery() when( @i < @max ) {
-			@media ( max-width: @max-width ) and ( min-width: @min-width ) {
+		.mediaquery() when ( @i > @min ) and ( @i < @max ) {
+			@media ( min-width: @min-width ) and ( max-width: @max-width ) {
 				.rules();
 			}
 		}
 
 		// no max-width constraint on the last media query, so the items can scale from that point
-		.mediaquery() when( @i = @max ) {
+		.mediaquery() when ( @i = @max ) {
 			@media ( min-width: @min-width ) {
 				.rules();
 			}

commit 00bd3c6d1f6034ca4aed725941930a2767ef6a40

Fix fetch limit calculation in InfiniteScrollMixin
caused by e74ef35

src/app/mixins/InfiniteScrollMixin.js
@@ -9,6 +9,7 @@ import {
 
 var CSSMediaRule     = window.CSSMediaRule;
 var CSSStyleRule     = window.CSSStyleRule;
+// lessCSS sorts media queries alphabetically, so we need to reverse the order here
 var reMinWidth       = /^(?:\(max-width:\s*\d+px\)\s*and\s*)?\(min-width:\s*(\d+)px\)$/;
 var cachedMinWidths  = {};
 

src/styles/mixins.less
@@ -130,8 +130,9 @@
 		}
 
 		// the first media query should only have a max-width constraint
+		// also include the min-width constraint, so the InfiniteScrollMixin can match the query
 		.mediaquery() when ( @i = @min ) {
-			@media ( max-width: @max-width ) {
+			@media ( min-width: 1px ) and ( max-width: @max-width ) {
 				.rules();
 			}
 		}