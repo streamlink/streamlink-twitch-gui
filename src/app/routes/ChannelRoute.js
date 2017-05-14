fdef3f2a9277b194308c9036e4eff634f39d276e
bastimeyer <mail@bastimeyer.de>
2017-05-14 15:21:25 +0200

    Also try to find channels by their name

--- src/app/routes/ChannelRoute.js
+++ src/app/routes/ChannelRoute.js
@@ -6,31 +6,43 @@ import RefreshRouteMixin from "mixins/RefreshRouteMixin";
 import preload from "utils/preload";
 
 
-export default Route.extend( RefreshRouteMixin, {
-	model( params ) {
-		const store = get( this, "store" );
-		const id = params.channel;
+const reNum = /^\d+$/;
 
-		// try to find a stream record if the channel is broadcasting
-		return store.findRecord( "twitchStream", id, { reload: true } )
-			// return the stream and embedded channel record
-			.then( stream => ({
-				stream,
-				channel: get( stream, "channel" )
-			}), () => {
+
+export default Route.extend( RefreshRouteMixin, {
+	async model( params ) {
+		const store = get( this, "store" );
+		let { channel: id } = params;
+		let stream;
+		let channel;
+
+		if ( !reNum.test( id ) ) {
+			const user = await store.findRecord( "twitchUser", id );
+			try {
+				stream = await get( user, "stream" );
+			} catch ( e ) {}
+			channel = await get( user, "channel" );
+
+		} else {
+			try {
+				stream = await store.findRecord( "twitchStream", id, { reload: true } );
+				channel = get( stream, "channel" );
+			} catch ( e ) {
 				// let the stream record transition from root.loading into root.empty
 				// so that it can be reloaded later on... fixes #89
-				const stream = store.recordForId( "twitchStream", id );
-				stream._internalModel.notFound();
-
+				store.recordForId( "twitchStream", id )._internalModel.notFound();
 				// if the channel is not online, just find and return the channel record
-				return store.findRecord( "twitchChannel", id, { reload: true } )
-					.then( channel => ({ channel }) );
-			})
-			.then( preload([
-				"stream.preview.largeLatest",
-				"channel.logo",
-				"channel.video_banner"
-			]) );
+				channel = await store.findRecord( "twitchChannel", id, { reload: true } );
+			}
+		}
+
+		const model = { stream, channel };
+		await preload([
+			"stream.preview.largeLatest",
+			"channel.logo",
+			"channel.video_banner"
+		])( model );
+
+		return model;
 	}
 });
