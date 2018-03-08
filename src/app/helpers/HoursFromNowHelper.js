f0234943b756038964677c29a1093243186b44b9
bastimeyer <mail@bastimeyer.de>
2018-03-08 01:35:24 +0100

    i18n: Helpers

--- src/app/helpers/HoursFromNowHelper.js
+++ src/app/helpers/HoursFromNowHelper.js
@@ -1,3 +1,5 @@
+import { get } from "@ember/object";
+import { inject as service } from "@ember/service";
 import FromNowHelper from "helpers/-FromNowHelper";
 
 
@@ -7,50 +9,56 @@ const hour   = 60 * minute;
 const day    = 24 * hour;
 
 
-function formatMinutes( diff, short ) {
-	let minutes = Math.floor( diff / minute );
-
-	let strZero = !short && minutes < 10 ? "0" : "";
-	let strMinutes = minutes.toFixed( 0 );
-
-	return `${strZero}${strMinutes}m`;
-}
-
-function formatHours( diff, short ) {
-	let hours = Math.floor( diff / hour );
-	let remaining = diff % hour;
-
-	let strHours = hours.toFixed( 0 );
-	let strMinutes = !short && remaining > minute
-		? formatMinutes( remaining, true )
-		: "";
-
-	return `${strHours}h${strMinutes}`;
-}
-
-function formatDays( diff ) {
-	let days = Math.floor( diff / day );
-	let remaining = diff % day;
-
-	let strDays = days.toFixed( 0 );
-	let strHours = remaining > hour
-		? formatHours( remaining, true )
-		: "";
-
-	return `${strDays}d${strHours}`;
-}
-
-
 export default FromNowHelper.extend({
-	_compute( params ) {
-		let diff = Date.now() - params[0];
+	i18n: service(),
 
-		return diff < minute
-			? "just now"
-			: diff < hour
-				? formatMinutes( diff )
-				: diff < day
-					? formatHours( diff )
-					: formatDays( diff );
+	_compute( params ) {
+		const i18n = get( this, "i18n" );
+		const diff = Date.now() - params[0];
+
+		if ( diff < minute / 2 ) {
+			return i18n.t( "helpers.hours-from-now.now" ).toString();
+
+		} else if ( diff < hour ) {
+			const minutes = diff < minute
+				? 1
+				: Math.floor( diff / minute );
+
+			return i18n.t( "helpers.hours-from-now.minutes", {
+				minutes: minutes < 10
+					? `0${minutes}`
+					: minutes.toFixed( 0 )
+			}).toString();
+
+		} else if ( diff < day ) {
+			const remaining = diff % hour;
+
+			if ( remaining > minute ) {
+				return i18n.t( "helpers.hours-from-now.hours.extended", {
+					hours: Math.floor( diff / hour ).toFixed( 0 ),
+					minutes: Math.floor( remaining / minute ).toFixed( 0 )
+				}).toString();
+
+			} else {
+				return i18n.t( "helpers.hours-from-now.hours.simple", {
+					hours: Math.floor( diff / hour ).toFixed( 0 )
+				}).toString();
+			}
+
+		} else {
+			const remaining = diff % day;
+
+			if ( remaining > hour ) {
+				return i18n.t( "helpers.hours-from-now.days.extended", {
+					days: Math.floor( diff / day ).toFixed( 0 ),
+					hours: Math.floor( remaining / hour ).toFixed( 0 )
+				}).toString();
+
+			} else {
+				return i18n.t( "helpers.hours-from-now.days.simple", {
+					days: Math.floor( diff / day ).toFixed( 0 )
+				}).toString();
+			}
+		}
 	}
 });
--- src/test/tests/helpers/HoursFromNowHelper.js
+++ src/test/tests/helpers/HoursFromNowHelper.js
@@ -1,5 +1,7 @@
 import { moduleForComponent, test } from "ember-qunit";
 import { buildResolver, hbs } from "test-utils";
+import { I18nService } from "i18n-utils";
+import { setOwner } from "@ember/application";
 import sinon from "sinon";
 
 import HoursFromNowHelper from "helpers/HoursFromNowHelper";
@@ -8,7 +10,8 @@ import HoursFromNowHelper from "helpers/HoursFromNowHelper";
 moduleForComponent( "helpers/HoursFromNowHelper", {
 	integration: true,
 	resolver: buildResolver({
-		HoursFromNowHelper
+		HoursFromNowHelper,
+		I18nService
 	}),
 	beforeEach() {
 		this.fakeTimer = sinon.useFakeTimers({
@@ -25,6 +28,7 @@ moduleForComponent( "helpers/HoursFromNowHelper", {
 test( "Unit test", function( assert ) {
 
 	const helper = HoursFromNowHelper.create();
+	setOwner( helper, this.container );
 
 	const time = new Date();
 	const minute = 60 * 1000;
@@ -35,37 +39,94 @@ test( "Unit test", function( assert ) {
 		return helper.compute( [ time ], {} );
 	}
 
-	assert.strictEqual( compute(), "just now", "Initial content" );
+	assert.strictEqual( compute(), "helpers.hours-from-now.now", "Initial content" );
 
-	this.fakeTimer.setSystemTime( minute - 1 );
-	assert.strictEqual( compute(), "just now", "Almost a minute" );
+	this.fakeTimer.setSystemTime( ( minute / 2 ) - 1 );
+	assert.strictEqual( compute(), "helpers.hours-from-now.now", "Almost half a minute" );
 
 	this.fakeTimer.tick( 1 );
-	assert.strictEqual( compute(), "01m", "One minute" );
+	assert.strictEqual(
+		compute(),
+		"helpers.hours-from-now.minutes{\"minutes\":\"01\"}",
+		"Half a minute"
+	);
+
+	this.fakeTimer.setSystemTime( minute );
+	assert.strictEqual(
+		compute(),
+		"helpers.hours-from-now.minutes{\"minutes\":\"01\"}",
+		"One minute"
+	);
 
 	this.fakeTimer.setSystemTime( 10 * minute - 1 );
-	assert.strictEqual( compute(), "09m", "Almost ten minutes" );
+	assert.strictEqual(
+		compute(),
+		"helpers.hours-from-now.minutes{\"minutes\":\"09\"}",
+		"Almost ten minutes"
+	);
 
 	this.fakeTimer.tick( 1 );
-	assert.strictEqual( compute(), "10m", "Ten minutes" );
+	assert.strictEqual(
+		compute(),
+		"helpers.hours-from-now.minutes{\"minutes\":\"10\"}",
+		"Ten minutes"
+	);
 
 	this.fakeTimer.setSystemTime( hour - 1 );
-	assert.strictEqual( compute(), "59m", "Almost an hour" );
+	assert.strictEqual(
+		compute(),
+		"helpers.hours-from-now.minutes{\"minutes\":\"59\"}",
+		"Almost an hour"
+	);
 
 	this.fakeTimer.tick( 1 );
-	assert.strictEqual( compute(), "1h", "One hour" );
+	assert.strictEqual(
+		compute(),
+		"helpers.hours-from-now.hours.simple{\"hours\":\"1\"}",
+		"One hour"
+	);
 
 	this.fakeTimer.setSystemTime( 10 * hour - 1 );
-	assert.strictEqual( compute(), "9h59m", "Almost ten hours" );
+	assert.strictEqual(
+		compute(),
+		"helpers.hours-from-now.hours.extended{\"hours\":\"9\",\"minutes\":\"59\"}",
+		"Almost ten hours"
+	);
 
 	this.fakeTimer.tick( 1 );
-	assert.strictEqual( compute(), "10h", "Ten hours" );
+	assert.strictEqual(
+		compute(),
+		"helpers.hours-from-now.hours.simple{\"hours\":\"10\"}",
+		"Ten hours"
+	);
 
 	this.fakeTimer.setSystemTime( day - 1 );
-	assert.strictEqual( compute(), "23h59m", "Almost a day" );
+	assert.strictEqual(
+		compute(),
+		"helpers.hours-from-now.hours.extended{\"hours\":\"23\",\"minutes\":\"59\"}",
+		"Almost a day"
+	);
 
 	this.fakeTimer.tick( 1 );
-	assert.strictEqual( compute(), "1d", "One day" );
+	assert.strictEqual(
+		compute(),
+		"helpers.hours-from-now.days.simple{\"days\":\"1\"}",
+		"One day"
+	);
+
+	this.fakeTimer.setSystemTime( 10 * day - 1 );
+	assert.strictEqual(
+		compute(),
+		"helpers.hours-from-now.days.extended{\"days\":\"9\",\"hours\":\"23\"}",
+		"Almost ten days"
+	);
+
+	this.fakeTimer.tick( 1 );
+	assert.strictEqual(
+		compute(),
+		"helpers.hours-from-now.days.simple{\"days\":\"10\"}",
+		"Ten days"
+	);
 
 });
 
@@ -75,13 +136,21 @@ test( "Interval", function( assert ) {
 	this.set( "time", new Date() );
 	this.render( hbs`{{hours-from-now time interval=60000}}` );
 
-	assert.strictEqual( this.$().text(), "just now", "Initial content" );
+	assert.strictEqual( this.$().text(), "helpers.hours-from-now.now", "Initial content" );
 
 	this.fakeTimer.tick( 59999 );
-	assert.strictEqual( this.$().text(), "just now", "Interval not yet completed" );
+	assert.strictEqual(
+		this.$().text(),
+		"helpers.hours-from-now.now",
+		"Interval not yet completed"
+	);
 
 	this.fakeTimer.tick( 1 );
-	assert.strictEqual( this.$().text(), "01m", "Updated content" );
+	assert.strictEqual(
+		this.$().text(),
+		"helpers.hours-from-now.minutes{\"minutes\":\"01\"}",
+		"Updated content"
+	);
 
 	this.clearRender();
 	this.fakeTimer.tick( 60000 );
