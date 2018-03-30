5e3362fbbe913208a756d8128c0b83c688a1f2a6
bastimeyer <mail@bastimeyer.de>
2018-03-30 16:43:37 +0200

    Upgrade ember-i18n and add default locale fallback

--- src/app/initializers/env.js
+++ src/app/initializers/env.js
@@ -1,13 +1,17 @@
 import { locales as localesConfig } from "config";
 
 
+const { default: defaultLocale } = localesConfig;
+
+
 export default {
 	name: "env",
 
 	initialize( application ) {
 		const ENV = {
 			i18n: {
-				defaultLocale: localesConfig[ "default" ]
+				defaultLocale,
+				defaultFallback: defaultLocale
 			}
 		};
 
