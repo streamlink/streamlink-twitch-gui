17f01c2c9a97c9eaccbb13c24c71833ef642b685
bastimeyer <mail@bastimeyer.de>
2018-03-31 18:09:46 +0200

    Sort language dropdown by localized language name

--- src/app/controllers/SettingsMainController.js
+++ src/app/controllers/SettingsMainController.js
@@ -9,11 +9,15 @@ const { themes } = themesConfig;
 
 export default Controller.extend({
 	contentGuiLanguages: computed(function() {
+		const compare = new Intl.Collator( "en", { sensitivity: "base" } ).compare;
+
 		return Object.keys( locales )
 			.map( key => ({
 				id: key,
 				label: locales[ key ]
-			}) );
+			}) )
+			// sort by localized language name in English order
+			.sort( ( a, b ) => compare( a.label, b.label ) );
 	}),
 
 	contentGuiTheme: computed(function() {
