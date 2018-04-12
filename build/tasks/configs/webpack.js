fd5e98cac0ada0ae8c3421dac54ea91b72e2fda3
bastimeyer <mail@bastimeyer.de>
2018-04-12 08:33:15 +0200

    Remove unused app resolve aliases

--- build/tasks/configs/webpack.js
+++ build/tasks/configs/webpack.js
@@ -19,7 +19,6 @@ const pConfig = r( pRoot, "config" );
 const pTest = r( pRoot, "test" );
 const pTestFixtures = r( pTest, "fixtures" );
 const pImages = r( pRoot, "img" );
-const pTemplates = r( pRoot, "templates" );
 const pDependencies = r( ".", "node_modules" );
 const pCacheBabel = r( tmpdir(), "babel-cache" );
 
@@ -297,21 +296,16 @@ module.exports = {
 				// folder aliases
 				"root"        : pRoot,
 				"img"         : pImages,
-				"templates"   : pTemplates,
 				"fixtures"    : pTestFixtures,
 
-				// app folders
+				// app aliases
 				"config"      : r( pApp, "config" ),
-				"nwjs"        : r( pApp, "nwjs" ),
-				"locales"     : r( pApp, "locales" ),
-				"init"        : r( pApp, "init" ),
-				"services"    : r( pApp, "services" ),
-				"helpers"     : r( pApp, "helpers" ),
 				"data"        : r( pApp, "data" ),
-				"models"      : r( pApp, "models" ),
+				"init"        : r( pApp, "init" ),
+				"locales"     : r( pApp, "locales" ),
+				"nwjs"        : r( pApp, "nwjs" ),
+				"services"    : r( pApp, "services" ),
 				"ui"          : r( pApp, "ui" ),
-				"components"  : r( pApp, "components" ),
-				"store"       : r( pApp, "store" ),
 				"utils"       : r( pApp, "utils" ),
 
 				// explicit lib/module paths
