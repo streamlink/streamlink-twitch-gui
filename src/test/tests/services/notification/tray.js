6f6118eec0575653e8234440ecc285cf88257930
bastimeyer <mail@bastimeyer.de>
2018-12-08 14:37:39 +0100

    new test API: NotificationService tray

--- src/test/tests/services/notification/tray.js
+++ src/test/tests/services/notification/tray.js
@@ -1,66 +1,70 @@
-import { moduleFor, test } from "ember-qunit";
+import { module, test } from "qunit";
+import { setupTest } from "ember-qunit";
 import { buildResolver } from "test-utils";
+
 import { A as EmberNativeArray } from "@ember/array";
-import { get, set } from "@ember/object";
+import { set } from "@ember/object";
 import { run } from "@ember/runloop";
 import Service from "@ember/service";
 
 import NotificationServiceTrayMixin from "services/notification/tray";
 
 
-moduleFor( "service:notification", "services/notification/tray", {
-	resolver: buildResolver({
-		NotificationService: Service.extend( NotificationServiceTrayMixin )
-	}),
-	beforeEach() {
+module( "services/notification/tray", function( hooks ) {
+	setupTest( hooks, {
+		resolver: buildResolver({
+			NotificationService: Service.extend( NotificationServiceTrayMixin )
+		})
+	});
+
+	hooks.beforeEach(function() {
 		this.items = new EmberNativeArray();
-		this.register( "service:nwjs", Service.extend({
+		this.owner.register( "service:nwjs", Service.extend({
 			addTrayMenuItem: item => this.items.unshiftObject( item ),
 			removeTrayMenuItem: item => this.items.removeObject( item )
 		}) );
-	}
-});
+	});
 
 
-test( "Tray menu item", function( assert ) {
+	test( "Tray menu item", async function( assert ) {
+		// stub NWjs behavior
+		const click = item => {
+			item.checked = !item.checked;
+			item.click( item );
+		};
 
-	// stub NWjs behavior
-	const click = item => {
-		item.checked = !item.checked;
-		item.click( item );
-	};
+		const service = this.owner.lookup( "service:notification" );
+		assert.strictEqual( service._trayMenuItem, null, "Doesn't create an item initially" );
 
-	const service = this.subject();
-	assert.strictEqual( get( service, "_trayMenuItem" ), null, "Doesn't create an item initially" );
+		// enable
+		run( () => set( service, "enabled", true ) );
+		assert.propEqual(
+			this.items.toArray(),
+			[{
+				type   : "checkbox",
+				label  : [ "services.notification.tray.pause.label" ],
+				tooltip: [ "services.notification.tray.pause.tooltip" ],
+				checked: false,
+				click  : () => {}
+			}],
+			"Creates a new tray icon context menu item"
+		);
+		assert.strictEqual( service._trayMenuItem, this.items[0], "Caches the menu item" );
 
-	// enable
-	run( () => set( service, "enabled", true ) );
-	assert.propEqual(
-		this.items.toArray(),
-		[{
-			type   : "checkbox",
-			label  : [ "services.notification.tray.pause.label" ],
-			tooltip: [ "services.notification.tray.pause.tooltip" ],
-			checked: false,
-			click  : () => {}
-		}],
-		"Creates a new tray icon context menu item"
-	);
-	assert.strictEqual( get( service, "_trayMenuItem" ), this.items[0], "Caches the menu item" );
+		// click
+		assert.strictEqual( service.paused, false, "Is not paused initially" );
+		click( this.items[0] );
+		assert.strictEqual( service.paused, true, "Is paused after clicking once" );
+		click( this.items[0] );
+		assert.strictEqual( service.paused, false, "Is not paused after clicking twice" );
+		click( this.items[0] );
+		assert.strictEqual( service.paused, true, "Is paused again" );
 
-	// click
-	assert.strictEqual( get( service, "paused" ), false, "Is not paused initially" );
-	click( this.items[0] );
-	assert.strictEqual( get( service, "paused" ), true, "Is paused after clicking once" );
-	click( this.items[0] );
-	assert.strictEqual( get( service, "paused" ), false, "Is not paused after clicking twice" );
-	click( this.items[0] );
-	assert.strictEqual( get( service, "paused" ), true, "Is paused again" );
-
-	// disable
-	run( () => set( service, "enabled", false ) );
-	assert.propEqual( this.items.toArray(), [], "Removes the menu item" );
-	assert.strictEqual( get( service, "_trayMenuItem" ), null, "Removes menu item from cache" );
-	assert.strictEqual( get( service, "paused" ), false, "Is not paused anymore" );
+		// disable
+		run( () => set( service, "enabled", false ) );
+		assert.propEqual( this.items.toArray(), [], "Removes the menu item" );
+		assert.strictEqual( service._trayMenuItem, null, "Removes menu item from cache" );
+		assert.strictEqual( service.paused, false, "Is not paused anymore" );
+	});
 
 });
