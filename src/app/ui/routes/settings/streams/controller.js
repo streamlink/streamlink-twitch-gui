676d59226ac917276b7881ca7cf923267a6e1ec3
bastimeyer <mail@bastimeyer.de>
2020-02-29 06:50:34 +0100

    Merge SettingsListsRoute with SettingsStreamsRoute
    This is the easiest way of fixing the SettingsRoute's sub menu width

--- src/app/ui/routes/settings/streams/controller.js
+++ src/app/ui/routes/settings/streams/controller.js
@@ -1,10 +1,26 @@
 import Controller from "@ember/controller";
-import { qualities } from "data/models/stream/model";
-import { DEFAULT_VODCAST_REGEXP } from "data/models/settings/streams/fragment";
+import { qualities as contentStreamingQuality } from "data/models/stream/model";
+import {
+	default as SettingsStreams,
+	DEFAULT_VODCAST_REGEXP
+} from "data/models/settings/streams/fragment";
+import { isDarwin } from "utils/node/platform";
+
+
+const {
+	contentName: contentStreamsName,
+	info: contentStreamsInfo,
+	click: contentStreamsClick
+} = SettingsStreams;
 
 
 export default Controller.extend({
-	contentStreamingQuality: qualities,
+	contentStreamingQuality,
+	contentStreamsName,
+	contentStreamsInfo,
+	contentStreamsClick,
 
-	DEFAULT_VODCAST_REGEXP
+	DEFAULT_VODCAST_REGEXP,
+
+	isDarwin
 });
