1e7db5b814322567b05ad24f9dd1615601ce0a5f
bastimeyer <mail@bastimeyer.de>
2020-03-04 20:24:00 +0100

    Add missing keyboard-layout-map initializer test

--- src/app/init/initializers/keyboard-layout-map.js
+++ src/app/init/initializers/keyboard-layout-map.js
@@ -10,17 +10,13 @@
 export default {
 	name: "keyboard-layout-map",
 
-	/* istanbul ignore next */
 	/** @param {Ember.Application} application */
 	initialize( application ) {
 		application.deferReadiness();
 
 		Promise.resolve()
 			.then( () => navigator.keyboard.getLayoutMap() )
-			.then(
-				layoutMap => layoutMap,
-				() => new Map()
-			)
+			.catch( () => new Map() )
 			.then( /** @param {KeyboardLayoutMap} layoutMap */ layoutMap => {
 				application.register( "keyboardlayoutmap:main", layoutMap, { instantiate: false } );
 				application.advanceReadiness();
