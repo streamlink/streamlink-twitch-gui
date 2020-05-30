0e7c2b68e7fab88c00d3defbf3994fae05f91a0b
bastimeyer <mail@bastimeyer.de>
2020-05-30 11:48:05 +0200

    build: fix deterministic tar & zip archives

--- build/tasks/common/archive.js
+++ build/tasks/common/archive.js
@@ -8,7 +8,7 @@ function tarGzipArchive({ input, prefix, output, now }) {
 		"&& find . -type f -print0 | LC_ALL=C sort -z",
 		// deterministic archive: explicitly set mtime and owner/group IDs+perms
 		// https://wiki.debian.org/ReproducibleBuilds/TimestampsInTarball
-		"| tar --null --files-from -",
+		"| tar --no-recursion --null --files-from -",
 		`"--mtime=@\${SOURCE_DATE_EPOCH:-${now}}"`,
 		"--numeric-owner --owner=0 --group=0",
 		"--mode=go=rX,u+rw,a-s",
@@ -16,6 +16,8 @@ function tarGzipArchive({ input, prefix, output, now }) {
 		// set flags=r to ignore prefix when archiving symlinks
 		// https://stackoverflow.com/a/29661783
 		`--transform 'flags=r;s,^\./,${prefix}/,'`,
+		// PAX headers
+		"--pax-option=exthdr.name=%d/PaxHeaders/%f,delete=atime,delete=ctime",
 		"--create",
 		// https://wiki.debian.org/ReproducibleBuilds/TimestampsInGzipHeaders
 		"| gzip --no-name --best",
@@ -28,18 +30,21 @@ function zipArchive({ input, prefix, output, now }) {
 	return [
 		// create and go into a temp dir
 		"cd \"$(mktemp --directory)\"",
-		// create a symbolic link used as the archive's prefix
-		`&& ln --symbolic '${input}' '${prefix}'`,
+		// create the archive's prefix
+		`&& mkdir '${prefix}'`,
+		`&& cp -a '${input}/.' '${prefix}/'`,
+		// fix file permissions
+		"&& find . -type d -exec chmod 0755 '{}' '+'",
+		"&& find . -type f -exec chmod 0644 '{}' '+'",
 		// deterministic archive: update the mtime of all dirs and files and the prefix
 		// https://wiki.debian.org/ReproducibleBuilds/TimestampsInZip
-		`&& find '${input}' '${prefix}' -print0`,
-		`| xargs -r0 touch --no-dereference "--date=@\${SOURCE_DATE_EPOCH:-${now}}"`,
+		`&& find . -exec touch --no-dereference "--date=@\${SOURCE_DATE_EPOCH:-${now}}" '{}' '+'`,
 		// include all files from the input dir via the prefix (mind the trailing /)
-		`&& find '${prefix}/' -type f | LC_ALL=C sort`,
+		`&& find '${prefix}/' | LC_ALL=C sort`,
 		// create the compressed archive and write to the output file
-		`| TZ=UTC zip --quiet -9 -X -@ '${output}'`,
-		// lastly remove the symbolic link and temp dir again
-		`&& rm '${prefix}'`,
+		`| TZ=UTC zip --quiet --latest-time -9 -X -@ '${output}'`,
+		// lastly remove the prefix and temp dir again
+		`&& rm -rf '${prefix}'`,
 		"&& rmdir \"$PWD\""
 	].join( " " );
 }
