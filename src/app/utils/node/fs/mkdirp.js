9affb9091cdd6f46362d8d746f9a9807cf4084b2
bastimeyer <mail@bastimeyer.de>
2020-09-03 18:46:13 +0200

    utils: switch to native mkdirp and rewrite tests

--- src/app/utils/node/fs/mkdirp.js
+++ src/app/utils/node/fs/mkdirp.js
@@ -1,56 +1,12 @@
-import { stat, isDirectory } from "utils/node/fs/stat";
-import { isWin } from "utils/node/platform";
-import { dirname } from "path";
-import { mkdir, W_OK } from "fs";
-import { promisify } from "util";
+import { mkdir } from "fs/promises";
 
 
 /**
- * @type {Function}
+ * @param {string} path
+ * @param {Object?} options
+ * @param {(string|number)?} options.mode
  * @returns {Promise}
  */
-const fsMkdir = promisify( mkdir );
-
-
-export function check( stats ) {
-	return isDirectory( stats )
-	    && ( isWin || ( stats.mode & W_OK ) > 0 );
+export default async function mkdirp( path, options = {} ) {
+	return mkdir( path, Object.assign( {}, options, { recursive: true } ) );
 }
-
-
-/**
- * simplified and promisified version of node-mkdirp
- * https://github.com/substack/node-mkdirp
- * @param {string} dir
- * @returns {Promise}
- */
-async function mkdirp( dir ) {
-	try {
-		// try to create directory
-		await fsMkdir( dir );
-
-	} catch ( err ) {
-		// directory already exists
-		if ( err && err.code === "EEXIST" ) {
-			return dir;
-		}
-
-		// recursively try to create the parent folder
-		if ( err && err.code === "ENOENT" ) {
-			const parent = dirname( dir );
-			if ( parent === dir ) {
-				throw new Error( "Can't create parent folder" );
-			}
-
-			await mkdirp( parent );
-			// try the current folder again
-			return await fsMkdir( dir );
-		}
-
-		// check if current directory is a directory and whether it is writable
-		return await stat( dir, check );
-	}
-}
-
-
-export default mkdirp;
--- src/test/tests/utils/node/fs/mkdirp.js
+++ src/test/tests/utils/node/fs/mkdirp.js
@@ -1,189 +1,85 @@
 import { module, test } from "qunit";
 import sinon from "sinon";
-import { R_OK, W_OK } from "fs";
-import { posix as path } from "path";
 
-import mkdirpInjector from "inject-loader?-util!utils/node/fs/mkdirp";
+import mkdirpInjector from "inject-loader!utils/node/fs/mkdirp";
 
 
-module( "utils/node/fs/mkdirp", {
-	beforeEach() {
-		this.statStub = sinon.stub();
-		this.fsMkdirStub = sinon.stub();
+module( "utils/node/fs/mkdirp", function( hooks ) {
+	/** @typedef {Object} TestContextUtilsNodeFsMkdirp */
 
-		this.subject = isWin => mkdirpInjector({
-			"utils/node/platform": {
-				isWin: !!isWin
-			},
-			"utils/node/fs/stat": {
-				isDirectory: stats => stats.isDirectory(),
-				stat: this.statStub
-			},
-			path,
-			fs: {
-				W_OK,
-				mkdir: this.fsMkdirStub
+	hooks.beforeEach( /** @this {TestContextUtilsNodeFsMkdirp} */ function() {
+		this.mkdirStub = sinon.stub().callsFake( path => Promise.resolve( path ) );
+
+		/** @type {Function} subject */
+		this.subject = mkdirpInjector({
+			"fs/promises": {
+				mkdir: this.mkdirStub
 			}
-		});
-	}
-});
-
-
-test( "No directory", async function( assert ) {
-
-	const errorMkdir = new Error( "mkdir fail" );
-	const errorStat = new Error( "stat fail" );
-
-	const { default: mkdirp } = this.subject();
-
-	this.statStub.rejects( errorStat );
-	this.fsMkdirStub.callsFake( ( dir, callback ) => callback( errorMkdir ) );
-
-	await assert.rejects( mkdirp( "/foo/bar" ), errorStat, "Rejects on existing file" );
-	assert.ok( this.fsMkdirStub.calledWith( "/foo/bar" ), "/foo/bar", "Tries to create directory" );
-	assert.ok( this.statStub.calledWith( "/foo/bar" ), "Tries to validate directory" );
-
-});
-
-
-test( "Already existing directory", async function( assert ) {
-
-	const error = new Error();
-	error.code = "EEXIST";
-
-	const { default: mkdirp } = this.subject();
-
-	this.fsMkdirStub.callsFake( ( dir, callback ) => callback( error ) );
-
-	const dir = await mkdirp( "/foo/bar" );
-	assert.strictEqual( dir, "/foo/bar", "Resolves with correct path" );
-	assert.ok( this.fsMkdirStub.calledWith( "/foo/bar" ), "/foo/bar", "Tries to create directory" );
-	assert.notOk( this.statStub.called, "Doesn't call stat" );
-
-});
-
-
-test( "New directory", async function( assert ) {
-
-	const existing = [ "/foo" ];
-	const error = new Error();
-	error.code = "ENOENT";
-
-	const { default: mkdirp } = this.subject();
-
-	this.fsMkdirStub.callsFake( ( dir, callback ) => {
-		// parent does not exist
-		if ( !existing.includes( path.dirname( dir ) ) ) {
-			callback( error );
-		} else {
-			// create new folder
-			existing.push( dir );
-			callback( null, dir );
-		}
+		})[ "default" ];
 	});
 
-	const dir = await mkdirp( "/foo/bar/baz/qux" );
-	assert.strictEqual( dir, "/foo/bar/baz/qux", "Returns path" );
-	assert.propEqual(
-		this.fsMkdirStub.args.map( args => args[0] ),
-		[
-			"/foo/bar/baz/qux",
-			"/foo/bar/baz",
-			"/foo/bar",
-			"/foo/bar/baz",
-			"/foo/bar/baz/qux"
-		],
-		"Creates all necessary parent directories"
-	);
-	assert.propEqual(
-		existing,
-		[
-			"/foo",
-			"/foo/bar",
-			"/foo/bar/baz",
-			"/foo/bar/baz/qux"
-		],
-		"Checks all required parent directories"
-	);
-	assert.notOk( this.statStub.called, "Doesn't call stat" );
 
-});
-
-
-test( "Non-writable parent directory", async function( assert ) {
-
-	const errorStat = new Error( "stat fail" );
-
-	const { default: mkdirp } = this.subject();
-
-	this.statStub.rejects( errorStat );
-	this.fsMkdirStub.callsFake( ( dir, callback ) => {
-		const err = new Error();
-		// parent dir /foo exists, but is not writable
-		err.code = dir === "/foo"
-			? "EACCES"
-			: "ENOENT";
-		callback( err );
+	test( "Error", async function( assert ) {
+		/** @this {TestContextUtilsNodeFsMkdirp} */
+		const err = new Error( "foo" );
+		this.mkdirStub.rejects( err );
+		await assert.rejects(
+			this.subject( "/foo/bar" ),
+			err,
+			"Rejects with error"
+		);
+		assert.propEqual(
+			this.mkdirStub.args,
+			[[ "/foo/bar", { recursive: true } ]],
+			"Sets the recursive option to true"
+		);
 	});
 
-	await assert.rejects(
-		mkdirp( "/foo/bar" ),
-		errorStat,
-		"Rejects if parent dir not writable"
-	);
-	assert.propEqual(
-		this.fsMkdirStub.args.map( args => args[0] ),
-		[ "/foo/bar", "/foo" ],
-		"Tries to create all parent directories"
-	);
-	assert.ok( this.statStub.calledWith( "/foo" ), "Checks whether parent dir is writable" );
+	test( "Simple parameters", async function( assert ) {
+		/** @this {TestContextUtilsNodeFsMkdirp} */
+		assert.strictEqual(
+			await this.subject( "/foo/bar" ),
+			"/foo/bar",
+			"Returns the created path"
+		);
+		assert.propEqual(
+			this.mkdirStub.args,
+			[[ "/foo/bar", { recursive: true } ]],
+			"Sets the recursive option to true"
+		);
+	});
 
-});
+	test( "Mode option", async function( assert ) {
+		/** @this {TestContextUtilsNodeFsMkdirp} */
+		assert.strictEqual(
+			await this.subject( "/foo/bar", { mode: 0o111 } ),
+			"/foo/bar",
+			"Returns the created path"
+		);
+		assert.propEqual(
+			this.mkdirStub.args,
+			[[ "/foo/bar", { recursive: true, mode: 0o111 } ]],
+			"Sets the recursive option to true and keeps the mode option"
+		);
+	});
 
-
-test( "Directory check function", async function( assert ) {
-
-	const { check: checkPosix } = this.subject( false );
-	const { check: checkWin } = this.subject( true );
-
-	assert.ok(
-		checkPosix({
-			mode: R_OK | W_OK,
-			isDirectory() { return true; }
-		}),
-		"Writable dir on POSIX"
-	);
-
-	assert.notOk(
-		checkPosix({
-			mode: R_OK,
-			isDirectory() { return true; }
-		}),
-		"Non-writable dir on POSIX"
-	);
-
-	assert.notOk(
-		checkPosix({
-			mode: W_OK,
-			isDirectory() { return false; }
-		}),
-		"Non-dir on POSIX"
-	);
-
-	assert.ok(
-		checkWin({
-			mode: R_OK,
-			isDirectory() { return true; }
-		}),
-		"Dir on Windows"
-	);
-
-	assert.notOk(
-		checkWin({
-			mode: R_OK,
-			isDirectory() { return false; }
-		}),
-		"Non-dir on Windows"
-	);
-
+	test( "Invalid options", async function( assert ) {
+		/** @this {TestContextUtilsNodeFsMkdirp} */
+		const options = { recursive: false, foo: "bar" };
+		assert.strictEqual(
+			await this.subject( "/foo/bar", options ),
+			"/foo/bar",
+			"Returns the created path"
+		);
+		assert.propEqual(
+			this.mkdirStub.args,
+			[[ "/foo/bar", { recursive: true, foo: "bar" } ]],
+			"Always sets the recursive option to true and keeps other options"
+		);
+		assert.propEqual(
+			options,
+			{ recursive: false, foo: "bar" },
+			"Doesn't alter the input options properties"
+		);
+	});
 });
