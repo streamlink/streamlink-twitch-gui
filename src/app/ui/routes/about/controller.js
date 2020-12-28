248a24b5bef4b3049990c9998880c6ff79fa24cd
JohanDelValleV <johandvv@gmail.com>
2020-12-28 13:39:40 -0600

    routes/about: add platform and release information
    Co-authored-by: bastimeyer <mail@bastimeyer.de>

--- src/app/ui/routes/about/controller.js
+++ src/app/ui/routes/about/controller.js
@@ -2,7 +2,7 @@ import Controller from "@ember/controller";
 import { main as mainConfig, locales as localesConfig } from "config";
 import metadata from "metadata";
 import { manifest } from "nwjs/App";
-import { arch } from "utils/node/platform";
+import { platform, release, arch } from "utils/node/platform";
 import "./styles.less";
 
 
@@ -10,6 +10,8 @@ export default Controller.extend({
 	mainConfig,
 	localesConfig,
 	metadata,
+	platform,
+	release,
 	arch,
 	releaseUrl: mainConfig.urls.release.replace( "{version}", manifest.version )
 });
