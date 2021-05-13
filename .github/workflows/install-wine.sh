10314d7f14f01103ccc340d711937484bf03c551
bastimeyer <mail@bastimeyer.de>
2021-05-13 04:20:38 +0200

    ci/github: switch to ubuntu-20.04 and fix wine

--- .github/workflows/install-wine.sh
+++ .github/workflows/install-wine.sh
@@ -10,16 +10,14 @@ sudo dpkg --add-architecture i386
 
 # add apt repository for latest wine builds from winehq
 curl -sSL https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
-sudo apt-add-repository "deb http://dl.winehq.org/wine-builds/ubuntu/ bionic main"
-
-# add apt repository for faudio workaround on bionic beaver
-curl -sSL https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/Release.key | sudo apt-key add -
-sudo apt-add-repository "deb https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/ ./"
+sudo apt-add-repository "deb https://dl.winehq.org/wine-builds/ubuntu/ focal main"
 
 # install winehq-stable package
-sudo apt-get update -qq && sudo apt-get install -qq -o=Dpkg::Use-Pty=0 --install-recommends -y winehq-stable
+sudo apt-get update -qq
+sudo apt-get install --install-recommends -y winehq-stable
 
 # boot wine prefix without install popup and manually download+install wine-mono (would otherwise hang due to Xvfb)
 WINEDLLOVERRIDES="mscoree,mshtml=" wineboot -i
-curl -sSL -o /tmp/wine-mono-5.0.0-x86.msi https://dl.winehq.org/wine/wine-mono/5.0.0/wine-mono-5.0.0-x86.msi
-wine msiexec /i /tmp/wine-mono-5.0.0-x86.msi
+curl -sSLO https://dl.winehq.org/wine/wine-mono/5.0.0/wine-mono-5.0.0-x86.msi
+sha256sum -c <<< "17da208645a82a5e45e84fc75c73a8440acda484411cb8fae8e9b72db9886cd5  wine-mono-5.0.0-x86.msi"
+wine msiexec /i wine-mono-5.0.0-x86.msi
