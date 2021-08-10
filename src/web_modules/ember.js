32b4ff90f1dc9670f2485e71cc26936b0f77eec2
bastimeyer <mail@bastimeyer.de>
2021-08-10 16:22:37 +0200

    ember: disable/remove most prototype extensions

--- src/web_modules/ember.js
+++ src/web_modules/ember.js
@@ -1,5 +1,13 @@
 window.EmberENV = Object.assign( window.EmberENV || {}, {
-	_JQUERY_INTEGRATION: false
+	_JQUERY_INTEGRATION: false,
+	EXTEND_PROTOTYPES: {
+		// TODO: disable Array.prototype extensions
+		Array: true,
+		Date: false,
+		// TODO: disable Function.prototype extensions
+		Function: true,
+		String: false
+	}
 });
 
 
@@ -13,6 +21,22 @@ window.requirejs = {
 	entries: Ember.__loader.registry
 };
 window.requireModule = Ember.__loader.require;
+
+
+// Remove unused deprecated prototype extensions which can't be disabled as a whole.
+// Sinon will access all properties on the global class prototypes and cause a deprecation warning.
+for ( const [ proto, props ] of new Map([
+	[ Array.prototype, [ "@each" ] ]
+]) ) {
+	for ( const prop of props ) {
+		Object.defineProperty( proto, prop, {
+			enumerable: false,
+			configurable: false,
+			writable: false,
+			value: null
+		});
+	}
+}
 
 
 export default Ember;
