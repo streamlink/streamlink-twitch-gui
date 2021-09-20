41f94a919d2ecdcc2eb0acd57cb081b0c71bfa8e
bastimeyer <mail@bastimeyer.de>
2021-09-20 08:22:02 +0200

    vendor: update QUnit to 2.17.2
    - update QUnit to latest version, which fixes checkboxes
    - fix rerun test anchors
    - fix type warning in test/main

--- src/test/main.js
+++ src/test/main.js
@@ -6,24 +6,35 @@ import "qunit/assertion-helpers";
 import "./web_modules/ember-test";
 
 
-// TODO: fix QUnit's urlParams / broken checkbox states
 const { config } = QUnit;
-
 
 // don't start automatically
 config.autostart = false;
-// check for pollution of the global scope (window)
-config.noglobals = true;
+
+// urlParams
+// TODO: fix this once QUnit implements default config values instead of forced overrides
+
 // hide passed tests
 config.hidepassed = true;
+// check for pollution of the global scope (window)
+config.noglobals = true;
 
 
 // load tests and then start
 require( [ "tests" ], /* istanbul ignore next */ function() {
 	if ( global._noQUnitBridge ) {
+		// fix broken "Rerun" anchor on completed tests
+		document.documentElement.addEventListener( "click", function( ev ) {
+			if ( ev.target instanceof HTMLAnchorElement && ev.target.closest( "#qunit-tests" ) ) {
+				ev.stopImmediatePropagation();
+				document.location.href = ev.target.href;
+			}
+		}, { capture: true } );
+
 		return QUnit.start();
 	}
-	if ( global._setupQUnitBridge ) {
+
+	if ( typeof global._setupQUnitBridge === "function" ) {
 		// bridge injected, set it up and start QUnit
 		global._setupQUnitBridge( QUnit );
 	} else {
