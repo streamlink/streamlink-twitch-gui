aa1aca5d09f019362d2fbe260b0ea6132316706f
bastimeyer <mail@bastimeyer.de>
2022-01-08 18:20:29 +0100

    ci: fix broken wine install
    resolves #850
    
    - add temporary workaround for dependency version conflicts
    - use wine-stable package from regular Ubuntu repositories
    - add wineserver --wait
    - delete downloaded wine-mono installer

--- .github/workflows/install-wine.sh
+++ .github/workflows/install-wine.sh
@@ -5,19 +5,26 @@ set -e
 
 set -x
 
-# install wine (needed for building Windows releases)
+# add i386 architecture
 sudo dpkg --add-architecture i386
 
-# add apt repository for latest wine builds from winehq
-curl -sSL https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
-sudo apt-add-repository "deb https://dl.winehq.org/wine-builds/ubuntu/ focal main"
+# fix version conflicts of certain library dependencies on GitHub's Ubuntu focal CI runner
+# https://github.com/actions/virtual-environments/issues/4589
+sudo apt-add-repository ppa:ondrej/php -y
 
-# install winehq-stable package
-sudo apt-get update -qq
-sudo apt-get install --install-recommends -y winehq-stable
+# install wine-stable from the regular Ubuntu package repositories
+sudo apt-get update
+sudo apt-get install --no-install-recommends -y wine-stable wine32
 
-# boot wine prefix without install popup and manually download+install wine-mono (would otherwise hang due to Xvfb)
+# boot wine prefix without install popup
 WINEDLLOVERRIDES="mscoree,mshtml=" wineboot -i
+wineserver --wait
+
+# manually download+install wine-mono (would otherwise hang due to Xvfb)
+# more recent versions of wine-mono will break anolis-resourcer which is used by nw-builder
+# for replacing the application's embedded icon bitmaps in the executable
 curl -sSLO https://dl.winehq.org/wine/wine-mono/5.0.0/wine-mono-5.0.0-x86.msi
 sha256sum -c <<< "17da208645a82a5e45e84fc75c73a8440acda484411cb8fae8e9b72db9886cd5  wine-mono-5.0.0-x86.msi"
 wine msiexec /i wine-mono-5.0.0-x86.msi
+wineserver --wait
+rm -f wine-mono-5.0.0-x86.msi
