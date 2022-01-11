c957d56f47a7ba2e0f7bfb7df2c33d4c939d337f
bastimeyer <mail@bastimeyer.de>
2022-01-11 20:05:47 +0100

    data: rewrite TwitchStreamFollowed

--- src/test/tests/data/models/twitch/stream-followed.js
+++ src/test/tests/data/models/twitch/stream-followed.js
@@ -1,108 +1,88 @@
 import { module, test } from "qunit";
-import { buildOwner, runDestroy } from "test-utils";
-import { setupStore, adapterRequest } from "store-utils";
+import { setupTest } from "ember-qunit";
+import { buildResolver } from "test-utils";
+import { adapterRequestFactory, assertRelationships, setupStore } from "store-utils";
 import { FakeIntlService } from "intl-utils";
-import { get } from "@ember/object";
+
 import Service from "@ember/service";
+import Model from "ember-data/model";
 
-import StreamFollowed from "data/models/twitch/stream-followed/model";
-import StreamFollowedSerializer from "data/models/twitch/stream-followed/serializer";
-import Stream from "data/models/twitch/stream/model";
-import StreamSerializer from "data/models/twitch/stream/serializer";
-import Channel from "data/models/twitch/channel/model";
-import ChannelSerializer from "data/models/twitch/channel/serializer";
-import imageInjector from "inject-loader?config!data/models/twitch/image/model";
-import ImageSerializer from "data/models/twitch/image/serializer";
 import TwitchAdapter from "data/models/twitch/adapter";
-import TwitchStreamFollowedFixtures from "fixtures/data/models/twitch/stream-followed.json";
+import TwitchStreamFollowed from "data/models/twitch/stream-followed/model";
+import TwitchStreamFollowedSerializer from "data/models/twitch/stream-followed/serializer";
+import TwitchStreamFollowedFixtures from "fixtures/data/models/twitch/stream-followed.yml";
+import TwitchStream from "data/models/twitch/stream/model";
+import TwitchStreamSerializer from "data/models/twitch/stream/serializer";
 
 
-const TwitchImage = imageInjector({
-	config: {
-		vars: {}
-	}
-})[ "default" ];
+module( "data/models/twitch/stream-followed", function( hooks ) {
+	setupTest( hooks, {
+		resolver: buildResolver({
+			IntlService: FakeIntlService,
+			SettingsService: Service.extend(),
+			TwitchStreamFollowed,
+			TwitchStreamFollowedSerializer,
+			TwitchStream,
+			TwitchStreamSerializer,
+			TwitchUser: Model.extend(),
+			TwitchChannel: Model.extend(),
+			TwitchGame: Model.extend()
+		})
+	});
+
+	hooks.beforeEach(function() {
+		setupStore( this.owner, { adapter: TwitchAdapter } );
+	});
 
 
-let owner, env;
+	test( "Model relationships", function( assert ) {
+		/** @type {DS.Store} */
+		const store = this.owner.lookup( "service:store" );
+		/** @type {TwitchStreamFollowed} */
+		const model = store.modelFor( "twitch-stream-followed" );
 
+		assertRelationships( assert, model, [
+			{
+				key: "stream",
+				kind: "belongsTo",
+				type: "twitch-stream",
+				options: { async: false }
+			}
+		]);
+	});
 
-module( "data/models/twitch/stream-followed", {
-	beforeEach() {
-		owner = buildOwner();
+	test( "query", async function( assert ) {
+		/** @type {DS.Store} */
+		const store = this.owner.lookup( "service:store" );
 
-		owner.register( "service:auth", Service.extend() );
-		owner.register( "service:intl", FakeIntlService );
-		owner.register( "model:twitch-stream-followed", StreamFollowed );
-		owner.register( "serializer:twitch-stream-followed", StreamFollowedSerializer );
-		owner.register( "model:twitch-stream", Stream );
-		owner.register( "serializer:twitch-stream", StreamSerializer );
-		owner.register( "model:twitch-channel", Channel );
-		owner.register( "serializer:twitch-channel", ChannelSerializer );
-		owner.register( "model:twitch-image", TwitchImage );
-		owner.register( "serializer:twitch-image", ImageSerializer );
+		store.adapterFor( "twitch-stream-followed" ).ajax
+			= adapterRequestFactory( assert, TwitchStreamFollowedFixtures );
 
-		env = setupStore( owner, { adapter: TwitchAdapter } );
-	},
+		const records = await store.query( "twitch-stream-followed", { user_id: "1" } );
 
-	afterEach() {
-		runDestroy( owner );
-		owner = env = null;
-	}
-});
-
-
-test( "Adapter and Serializer", assert => {
-
-	env.adapter.ajax = ( url, method, query ) =>
-		adapterRequest( assert, TwitchStreamFollowedFixtures, url, method, query );
-
-	return env.store.query( "twitchStreamFollowed", {} )
-		.then( records => {
-			assert.strictEqual(
-				get( records, "length" ),
-				2,
-				"Returns all records"
-			);
-
-			assert.strictEqual(
-				get( records, "meta.total" ),
-				1000,
-				"Recordarray has metadata with total number of games"
-			);
-
-			assert.deepEqual(
-				records.map( record => record.toJSON({ includeId: true }) ),
-				[
-					{
-						id: "2",
-						stream: "2"
-					},
-					{
-						id: "4",
-						stream: "4"
-					}
-				],
-				"Models have the correct id and attributes"
-			);
-
-			assert.ok(
-				   env.store.hasRecordForId( "twitchStream", "2" )
-				&& env.store.hasRecordForId( "twitchStream", "4" ),
-				"Has all Stream records registered in the data store"
-			);
-
-			assert.ok(
-				   env.store.hasRecordForId( "twitchChannel", "2" )
-				&& env.store.hasRecordForId( "twitchChannel", "4" ),
-				"Has all Channel records registered in the data store"
-			);
-
-			assert.ok(
-				   env.store.hasRecordForId( "twitchImage", "stream/preview/2" )
-				&& env.store.hasRecordForId( "twitchImage", "stream/preview/4" ),
-				"Has all Image records registered in the data store"
-			);
-		});
-
+		assert.propEqual(
+			records.map( record => record.toJSON({ includeId: true }) ),
+			[
+				{
+					id: "1",
+					stream: "1"
+				},
+				{
+					id: "2",
+					stream: "2"
+				}
+			],
+			"Records have the correct IDs and relationship IDs"
+		);
+		assert.propEqual(
+			store.peekAll( "twitch-stream-followed" ).mapBy( "id" ),
+			[ "1", "2" ],
+			"Has all TwitchStreamFollowed records registered in the data store"
+		);
+		assert.propEqual(
+			store.peekAll( "twitch-stream" ).mapBy( "id" ),
+			[ "1", "2" ],
+			"Has all TwitchStream records registered in the data store"
+		);
+	});
 });
