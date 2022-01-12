401c6e64b17710c7781f9caa1a59c9bb43a40ff8
bastimeyer <mail@bastimeyer.de>
2022-01-12 13:32:14 +0100

    data: rewrite TwitchSearchChannel

--- src/app/data/models/twitch/search-channel/model.js
+++ src/app/data/models/twitch/search-channel/model.js
@@ -1,10 +1,94 @@
+import { computed } from "@ember/object";
+import { inject as service } from "@ember/service";
+import attr from "ember-data/attr";
 import Model from "ember-data/model";
 import { belongsTo } from "ember-data/relationships";
+import { DEFAULT_VODCAST_REGEXP } from "data/models/settings/streams/fragment";
 
 
-export default Model.extend({
-	channel: belongsTo( "twitchChannel", { async: false } )
+// noinspection JSValidateTypes
+export default Model.extend( /** @class TwitchSearchChannel */ {
+	/** @type {IntlService} */
+	intl: service(),
+	/** @type {SettingsService} */
+	settings: service(),
+
+	/** @type {TwitchUser} */
+	user: belongsTo( "twitch-user", { async: true } ),
+	/** @type {string} */
+	broadcaster_language: attr( "string" ),
+	/** @type {string} */
+	broadcaster_login: attr( "string" ),
+	/** @type {string} */
+	display_name: attr( "string" ),
+	/** @type {string} */
+	game: belongsTo( "twitch-game", { async: true } ),
+	/** @type {string} */
+	game_name: attr( "string" ),
+	/** @type {boolean} */
+	is_live: attr( "boolean" ),
+	/** @type {string} */
+	thumbnail_url: attr( "string" ),
+	/** @type {string} */
+	title: attr( "string" ),
+	/** @type {Date} */
+	started_at: attr( "date" ),
+
+
+	/** @type {(null|RegExp)} */
+	reVodcast: computed(
+		"settings.content.streams.vodcast_regexp",
+		/** @this {TwitchStream} */
+		function() {
+			const vodcast_regexp = this.settings.content.streams.vodcast_regexp;
+
+			if ( vodcast_regexp.length && !vodcast_regexp.trim().length ) {
+				return null;
+			}
+			try {
+				return new RegExp( vodcast_regexp || DEFAULT_VODCAST_REGEXP, "i" );
+			} catch ( e ) {
+				return null;
+			}
+		}
+	),
+
+	/** @type {boolean} */
+	isVodcast: computed(
+		"reVodcast",
+		"title",
+		/** @this {TwitchStream} */
+		function() {
+			const { reVodcast, title } = this;
+
+			return reVodcast && title && reVodcast.test( title );
+		}
+	),
+
+	/** @type {string} */
+	titleStartedAt: computed(
+		"intl.locale",
+		"started_at",
+		/** @this {TwitchStream} */
+		function() {
+			const { started_at } = this;
+
+			return !started_at
+				? this.intl.t(
+					"models.twitch.search-channel.started-at.offline"
+				)
+				: new Date() - started_at < 24 * 3600 * 1000
+					? this.intl.t(
+						"models.twitch.search-channel.started-at.less-than-24h",
+						{ started_at }
+					)
+					: this.intl.t(
+						"models.twitch.search-channel.started-at.more-than-24h",
+						{ started_at }
+					);
+		}
+	)
 
 }).reopenClass({
-	toString() { return "kraken/search/channels"; }
+	toString() { return "helix/search/channels"; }
 });
--- src/test/tests/data/models/twitch/search-channel.js
+++ src/test/tests/data/models/twitch/search-channel.js
@@ -1,68 +1,227 @@
 import { module, test } from "qunit";
-import { buildOwner, runDestroy } from "test-utils";
-import { setupStore, adapterRequest } from "store-utils";
+import { setupTest } from "ember-qunit";
+import { buildResolver } from "test-utils";
+import { adapterRequestFactory, assertRelationships, setupStore } from "store-utils";
 import { FakeIntlService } from "intl-utils";
+import sinon from "sinon";
+
+import { set } from "@ember/object";
 import Service from "@ember/service";
 
-import SearchChannel from "data/models/twitch/search-channel/model";
-import SearchChannelSerializer from "data/models/twitch/search-channel/serializer";
-import Channel from "data/models/twitch/channel/model";
-import ChannelSerializer from "data/models/twitch/channel/serializer";
+import Model from "ember-data/model";
+
 import TwitchAdapter from "data/models/twitch/adapter";
-import TwitchSearchChannelFixtures from "fixtures/data/models/twitch/search-channel.json";
+import TwitchSearchChannel from "data/models/twitch/search-channel/model";
+import TwitchSearchChannelSerializer from "data/models/twitch/search-channel/serializer";
+import TwitchSearchChannelFixtures from "fixtures/data/models/twitch/search-channel.yml";
+import TwitchUser from "data/models/twitch/user/model";
+import TwitchUserAdapter from "data/models/twitch/user/adapter";
+import TwitchUserSerializer from "data/models/twitch/user/serializer";
+import TwitchGame from "data/models/twitch/game/model";
+import TwitchGameAdapter from "data/models/twitch/game/adapter";
+import TwitchGameSerializer from "data/models/twitch/game/serializer";
 
 
-let owner, env;
-
-
-module( "data/models/twitch/search-channel", {
-	beforeEach() {
-		owner = buildOwner();
-
-		owner.register( "service:auth", Service.extend() );
-		owner.register( "service:intl", FakeIntlService );
-		owner.register( "model:twitch-search-channel", SearchChannel );
-		owner.register( "serializer:twitch-search-channel", SearchChannelSerializer );
-		owner.register( "model:twitch-channel", Channel );
-		owner.register( "serializer:twitch-channel", ChannelSerializer );
-
-		env = setupStore( owner, { adapter: TwitchAdapter } );
-	},
-
-	afterEach() {
-		runDestroy( owner );
-		owner = env = null;
-	}
-});
-
-
-test( "Adapter and Serializer", assert => {
-
-	env.adapter.ajax = ( url, method, query ) =>
-		adapterRequest( assert, TwitchSearchChannelFixtures, url, method, query );
-
-	return env.store.query( "twitchSearchChannel", { query: "foo" } )
-		.then( records => {
-			assert.deepEqual(
-				records.map( record => record.toJSON({ includeId: true }) ),
-				[
-					{
-						id: "1",
-						channel: "1"
-					},
-					{
-						id: "2",
-						channel: "2"
+module( "data/models/twitch/search-channel", function( hooks ) {
+	setupTest( hooks, {
+		resolver: buildResolver({
+			IntlService: FakeIntlService,
+			SettingsService: Service.extend({
+				content: {
+					streams: {
+						vodcast_regexp: ""
 					}
-				],
-				"Models have the correct id and attributes"
-			);
+				}
+			}),
+			TwitchSearchChannel,
+			TwitchSearchChannelSerializer,
+			TwitchUser,
+			TwitchUserAdapter,
+			TwitchUserSerializer,
+			TwitchGame,
+			TwitchGameAdapter,
+			TwitchGameSerializer,
+			TwitchChannel: Model.extend()
+		})
+	});
 
-			assert.ok(
-				   env.store.hasRecordForId( "twitchChannel", "1" )
-				&& env.store.hasRecordForId( "twitchChannel", "2" ),
-				"Has all Channel records registered in the data store"
-			);
+	hooks.beforeEach(function() {
+		this.fakeTimer = sinon.useFakeTimers({
+			toFake: [ "Date" ],
+			global: window
 		});
 
+		setupStore( this.owner, { adapter: TwitchAdapter } );
+	});
+
+	hooks.afterEach(function() {
+		this.fakeTimer.restore();
+	});
+
+
+	test( "Module relationships", function( assert ) {
+		/** @type {DS.Store} */
+		const store = this.owner.lookup( "service:store" );
+		/** @type {TwitchSearchChannel} */
+		const model = store.modelFor( "twitch-search-channel" );
+
+		assertRelationships( assert, model, [
+			{
+				key: "user",
+				kind: "belongsTo",
+				type: "twitch-user",
+				options: { async: true }
+			},
+			{
+				key: "game",
+				kind: "belongsTo",
+				type: "twitch-game",
+				options: { async: true }
+			}
+		]);
+	});
+
+	test( "Computed properties - vodcast", function( assert ) {
+		/** @type {DS.Store} */
+		const store = this.owner.lookup( "service:store" );
+
+		/** @type {TwitchSearchChannel} */
+		const record = store.createRecord( "twitch-search-channel", {} );
+
+		assert.ok( record.reVodcast instanceof RegExp, "Has a default vodcast RegExp" );
+
+		set( record, "settings.content.streams.vodcast_regexp", " " );
+		assert.strictEqual( record.reVodcast, null, "Returns null on empty RegExp" );
+
+		set( record, "settings.content.streams.vodcast_regexp", "(" );
+		assert.strictEqual( record.reVodcast, null, "Returns null on invalid RegExp" );
+
+		set( record, "settings.content.streams.vodcast_regexp", "I'm a vodcast" );
+		assert.ok( record.reVodcast.test( "I'M A VODCAST" ), "Has a custom vodcast RegExp" );
+
+		assert.notOk( record.isVodcast, "Not a vodcast" );
+
+		set( record, "title", "I'm a vodcast" );
+		assert.ok( record.isVodcast, "Is a vodcast because of its title" );
+	});
+
+	test( "Computed properties - i18n", function( assert ) {
+		/** @type {DS.Store} */
+		const store = this.owner.lookup( "service:store" );
+
+		/** @type {TwitchSearchChannel} */
+		const record = store.createRecord( "twitch-search-channel", {} );
+
+		const day = 3600 * 24 * 1000;
+		this.fakeTimer.setSystemTime( 2 * day );
+
+		assert.strictEqual(
+			record.titleStartedAt,
+			"models.twitch.search-channel.started-at.offline",
+			"Shows an offline title for streams without started_at attribute"
+		);
+
+		set( record, "started_at", new Date( Date.now() - 1 ) );
+		assert.strictEqual(
+			record.titleStartedAt,
+			"models.twitch.search-channel.started-at.less-than-24h"
+			+ "{\"started_at\":\"1970-01-02T23:59:59.999Z\"}",
+			"Shows a shorthand title for streams running for less than 24h"
+		);
+
+		set( record, "started_at", new Date( Date.now() - day ) );
+		assert.strictEqual(
+			record.titleStartedAt,
+			"models.twitch.search-channel.started-at.more-than-24h"
+			+ "{\"started_at\":\"1970-01-02T00:00:00.000Z\"}",
+			"Shows an extended title for streams running for more than 24h"
+		);
+	});
+
+	test( "query", async function( assert ) {
+		/** @type {DS.Store} */
+		const store = this.owner.lookup( "service:store" );
+
+		const searchChannelResponseStub
+			= store.adapterFor( "twitch-search-channel" ).ajax
+			= adapterRequestFactory( assert, TwitchSearchChannelFixtures, "search-channel" );
+		const userResponseStub
+			= store.adapterFor( "twitch-user" ).ajax
+			= adapterRequestFactory( assert, TwitchSearchChannelFixtures, "user" );
+		const gameResponseStub
+			= store.adapterFor( "twitch-game" ).ajax
+			= adapterRequestFactory( assert, TwitchSearchChannelFixtures, "game" );
+
+		const records = await store.query( "twitch-search-channel", {
+			query: "foo",
+			live_only: true
+		});
+
+		assert.propEqual(
+			records.map( record => record.toJSON({ includeId: true }) ),
+			[
+				{
+					id: "1",
+					user: "1",
+					broadcaster_language: "en",
+					broadcaster_login: "foo",
+					display_name: "FOO",
+					game: "1",
+					game_name: "some game",
+					is_live: true,
+					thumbnail_url: "https://mock/twitch-search-channel/1/thumbnail-{width}x{height}.jpg",
+					title: "some title",
+					started_at: "2000-01-01T00:00:00.000Z"
+				},
+				{
+					id: "2",
+					user: "2",
+					broadcaster_language: "de",
+					broadcaster_login: "bar",
+					display_name: "BAR",
+					game: "2",
+					game_name: "another game",
+					is_live: true,
+					thumbnail_url: "https://mock/twitch-search-channel/2/thumbnail-{width}x{height}.jpg",
+					title: "another title",
+					started_at: "1999-12-31T23:59:59.000Z"
+				}
+			],
+			"All records have the correct IDs, attributes and relationship IDs"
+		);
+		assert.propEqual(
+			store.peekAll( "twitch-search-channel" ).mapBy( "id" ),
+			[ "1", "2" ],
+			"Has all TwitchSearchChannel records registered in the data store"
+		);
+		assert.propEqual(
+			store.peekAll( "twitch-user" ).mapBy( "id" ),
+			[],
+			"Has no TwitchUser record registered in the data store"
+		);
+		assert.propEqual(
+			store.peekAll( "twitch-game" ).mapBy( "id" ),
+			[],
+			"Has no TwitchGame record registered in the data store"
+		);
+		assert.ok( searchChannelResponseStub.calledOnce, "Queries API once for search-channels" );
+		assert.notOk( userResponseStub.called, "Hasn't queried API for user" );
+		assert.notOk( gameResponseStub.called, "Hasn't queried API for game" );
+
+		await Promise.all( records.map( record => record.user.promise ) );
+		assert.propEqual(
+			store.peekAll( "twitch-user" ).mapBy( "id" ),
+			[ "1", "2" ],
+			"Has all TwitchUser records registered in the data store"
+		);
+		assert.ok( userResponseStub.calledOnce, "Has queried API for user" );
+
+		await Promise.all( records.map( record => record.game.promise ) );
+		assert.propEqual(
+			store.peekAll( "twitch-game" ).mapBy( "id" ),
+			[ "1", "2" ],
+			"Has all TwitchGame records registered in the data store"
+		);
+		assert.ok( userResponseStub.calledOnce, "Has queried API for game" );
+	});
 });
