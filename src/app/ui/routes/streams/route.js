4cfb07870ed0537a2ec245bcc65acbd216f933c8
bastimeyer <mail@bastimeyer.de>
2022-01-18 06:07:15 +0100

    ui/routes: fix StreamsRoute
    - inherit from UserIndexRoute
    - fix infinite-scroll / pagination parameters
    - reformat template

--- src/app/ui/routes/streams/route.js
+++ src/app/ui/routes/streams/route.js
@@ -1,11 +1,22 @@
-import Route from "@ember/routing/route";
+import UserIndexRoute from "ui/routes/user/index/route";
 import PaginationMixin from "ui/routes/-mixins/routes/infinite-scroll/pagination";
 import FilterLanguagesMixin from "ui/routes/-mixins/routes/filter-languages";
 import RefreshRouteMixin from "ui/routes/-mixins/routes/refresh";
 
 
-export default Route.extend( PaginationMixin, FilterLanguagesMixin, RefreshRouteMixin, {
+export default UserIndexRoute.extend( PaginationMixin, FilterLanguagesMixin, RefreshRouteMixin, {
 	itemSelector: ".stream-item-component",
-	modelName: "twitchStream",
-	modelPreload: "preview.mediumLatest"
+	modelName: "twitch-stream",
+	modelPreload: "thumbnail_url.latest",
+
+	/**
+	 * @param {TwitchStream} twitchStream
+	 * @return {Promise}
+	 */
+	async modelItemLoader( twitchStream ) {
+		await Promise.all([
+			twitchStream.user.promise,
+			twitchStream.channel.promise
+		]);
+	}
 });
