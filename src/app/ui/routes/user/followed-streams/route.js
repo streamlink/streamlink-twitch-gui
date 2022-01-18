a76ebd2c3a491919f87c85d50c7576f5a2d62f1b
bastimeyer <mail@bastimeyer.de>
2022-01-18 06:09:52 +0100

    ui/routes: fix UserFollowedStreamsRoute
    - fix infinite-scroll / pagination parameters
    - remove custom model hook for the old kraken API bug and remove tests
    - reformat template

--- src/app/ui/routes/user/followed-streams/route.js
+++ src/app/ui/routes/user/followed-streams/route.js
@@ -1,66 +1,29 @@
-import { A } from "@ember/array";
-import UserIndexRoute from "../index/route";
+import UserIndexRoute from "ui/routes/user/index/route";
 import PaginationMixin from "ui/routes/-mixins/routes/infinite-scroll/pagination";
 import RefreshRouteMixin from "ui/routes/-mixins/routes/refresh";
-import { mapBy } from "utils/ember/recordArrayMethods";
-import { preload } from "utils/preload";
 
 
 export default UserIndexRoute.extend( PaginationMixin, RefreshRouteMixin, {
+	modelName: "twitch-stream-followed",
+	modelMapBy: "stream",
+	modelPreload: "thumbnail_url.latest",
 	itemSelector: ".stream-item-component",
 
-	// Guard against infinite API queries in case Twitch's query offset implemention breaks.
-	// Assume that nobody has followed so many channels that more than 500 (5*maxLimit) streams
-	// are online at the same time.
-	maxQueries: 5,
-
 	/**
-	 * Twitch doesn't sort the followed streams response anymore:
-	 * https://github.com/streamlink/streamlink-twitch-gui/issues/699
-	 *
-	 * @returns {Promise<TwitchStreamFollowed[]>}
+	 * @param {TwitchStream} twitchStream
+	 * @return {Promise}
 	 */
-	async model() {
-		const store = this.store;
-		const limit = this.maxLimit;
-
-		const maxQueries = this.maxQueries;
-		let i = 0;
-
-		const all = A();
-		let records;
-		let offset = 0;
-		do {
-			records = await store.query( "twitch-stream-followed", { offset, limit } );
-			offset += records.length;
-			all.push( ...mapBy( records, "stream" ) );
-		} while ( records.length >= limit && ++i <= maxQueries );
-
-		// make sure that no followed stream appears twice between API queries
-		this.all = all.uniqBy( "id" );
-		// finally sort by viewers locally
-		this.all.sort( ( a, b ) => b.viewers - a.viewers );
-
-		// return the first infinite scroll result as initial model data
-		return await this.fetchContent();
+	async modelItemLoader( twitchStream ) {
+		await Promise.all([
+			twitchStream.user.promise,
+			twitchStream.channel.promise
+		]);
 	},
 
-	/**
-	 * @returns {Promise<TwitchStreamFollowed[]>}
-	 */
-	async fetchContent() {
-		const { all, offset, limit } = this;
-		const streams = all.slice( offset, offset + limit );
-		// let InfiniteScrollMixin know when it has fetched all records
-		streams.meta = {
-			total: all.length
-		};
+	query() {
+		const query = this._super();
+		const { user_id } = this.auth.session;
 
-		return await preload( streams, "preview.mediumLatest" );
-	},
-
-	deactivate() {
-		this._super( ...arguments );
-		this.all = null;
+		return Object.assign( query, { user_id } );
 	}
 });
