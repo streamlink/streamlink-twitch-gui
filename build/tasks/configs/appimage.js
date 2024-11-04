9ad48dc39c2f37cd8a0e76b4a8658113fd500f93
bastimeyer <mail@bastimeyer.de>
2024-11-04 19:51:21 +0100

    build: bump appimage buildenv docker images
    Update to 20241003-1 (last CentOS 7 images)

--- build/tasks/configs/appimage.js
+++ build/tasks/configs/appimage.js
@@ -3,7 +3,7 @@
 module.exports = {
 	linux32: {
 		image: "ghcr.io/streamlink/appimage-buildenv-i686",
-		digest: "sha256:6d9218d0a2b7d755d9b54a9f8f58e6fe144f60c2961553896d7af1d7c9f3bb47",
+		digest: "sha256:c4ad9e44c413e8dd5e7746541e30b0b5a045a31fb3a2d09fcca6e3dcf4ef66b2",
 		input: "<%= dir.releases %>/<%= package.name %>/linux32",
 		output: "<%= dir.dist %>/<%= package.name %>-<%= version %>-i686.AppImage",
 		dependencies: [
@@ -12,7 +12,7 @@ module.exports = {
 	},
 	linux64: {
 		image: "ghcr.io/streamlink/appimage-buildenv-x86_64",
-		digest: "sha256:2f08ec73b4d593f365a14f3e0844beb16b334f1db42ca7c05e7a8095929d81db",
+		digest: "sha256:ef8a305ec5b35258c3025ac029693dd3e6a822f4983354eb8c2243e48d1ebc2e",
 		input: "<%= dir.releases %>/<%= package.name %>/linux64",
 		output: "<%= dir.dist %>/<%= package.name %>-<%= version %>-x86_64.AppImage",
 		dependencies: [
